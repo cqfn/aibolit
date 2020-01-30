3255e14f503ff3d0c197e69940827e977d3d4928
Yegor Bugayenko <yegor256@gmail.com>
2020-01-30 18:55:20 +0300

    #2 skeleton

---
+++ aibolit/__init__.py
@@ -0,0 +1,7 @@
+"""
+Aibolit: Defect Detection Static Analyzer with Machine Learning in Mind
+"""
+
+__version__ = '0.0.0'
+__author__ = 'Yegor Bugayenko'
+__licence__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,36 @@
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+setup(
+    name='aibolit',
+    version=aibolit.__version__,
+    description=aibolit.__doc__.strip(),
+    long_description=long_description(),
+    url='https://github.com/yegor256/aibolit',
+    download_url='https://github.com/yegor256/aibolit',
+    author=aibolit.__author__,
+    author_email='yegor256@gmail.com',
+    license=aibolit.__licence__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'http = aibolit.__main__:main'
+        ],
+    },
+    extras_require=[],
+    install_requires=[],
+    tests_require=[],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development',
+        'Topic :: Utilities'
+    ],
+)
