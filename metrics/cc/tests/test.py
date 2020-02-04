435d8599c980a59de2cebd6e6348654328eb2961
ALEXEY ZORCHENKOV <zorchenkov.alexey@huawei.com>
2020-02-04 12:09:24 +0000

    Task #7 Implement download java repositories script

--- metrics/cc/tests/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('/home/zuoqin/java/aibolit/metrics/cc')
-from main import CCMetric
-input = 'tests/anton.java'
-showlog = True
-metric = CCMetric(input, showlog)
-print(metric.value)
