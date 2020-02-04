63567c9f3fcabd6c9d71f2bee9e97636b7260721
ALEXEY ZORCHENKOV <zorchenkov.alexey@huawei.com>
2020-02-04 06:41:47 +0000

    Merge with origin master

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 import sys
+sys.path.append('./aibolit/metrics/cc')
 from main import CCMetric
-sys.path.append('/home/zuoqin/java/aibolit/metrics/cc')
 
-input = 'tests/anton.java'
+input = 'test/anton.java'
 showlog = True
 metric = CCMetric(input, showlog)
 print(metric.value)
