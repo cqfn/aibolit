d411288612dfe32567feeb9528b06c26c871287a
Yegor Bugayenko <yegor256@gmail.com>
2020-02-04 10:45:14 +0300

    #5 four spaces

--- test/metrics/loc/test_loc.py
+++ test/metrics/loc/test_loc.py
@@ -26,7 +26,7 @@ from aibolit.metrics.loc.loc import Loc
 
 
 class LocTest(TestCase):
-  def test_it_works(self):
-    m = Loc(os.path.dirname(os.path.realpath(__file__)) + '/sample-1.java')
-    loc = m.value()
-    self.assertEqual(loc, 5)
+    def test_it_works(self):
+        m = Loc(os.path.dirname(os.path.realpath(__file__)) + '/sample-1.java')
+        loc = m.value()
+        self.assertEqual(loc, 5)
