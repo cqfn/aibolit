00ba792b22b743ee21f751e6bf013969f629a360
Anton Cheshkov <anton.cheshkov@huawei.com>
2020-02-04 17:57:34 +0300

    license; test rename

--- test/patterns/for_nested/SingleFor.java
+++ test/patterns/for_nested/SingleFor.java
@@ -14,6 +14,10 @@ class Test {
         for (int i = 0; i < 10; i++)
             for (int i = 0; i < 10; i++)
                 list.add(Boolean.FALSE);
+
+        for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
+                list.add(Boolean.FALSE);
         final Destination destination = jndiService.locate(destinationName);
 
         this.publisher = jmsSession.createProducer(destination);
