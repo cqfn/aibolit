db66f6e2682962ad1251a831becb496972dfc49e
ALEXEY ZORCHENKOV <zorchenkov.alexey@huawei.com>
2020-02-04 08:34:45 +0000

    Merge with origin master

--- test/metrics/cc/test_all_types.py
+++ test/metrics/cc/test_all_types.py
@@ -1,4 +1,25 @@
-# coding=utf-8
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Aibolit
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import unittest
 
 
@@ -11,36 +32,36 @@ class JavaTestCase(unittest.TestCase):
         super(JavaTestCase, self).setUp()
         from aibolit.metrics.cc.main import CCMetric
 
-        metric = CCMetric('test/metrics/cc/javafiles/')
+        metric = CCMetric('test/metrics/cc/')
         res = metric.value(True)
-        data = list(filter(lambda x: x['file'] == 'test/metrics/cc/javafiles/Complicated.java', res['data']))
+        data = list(filter(lambda x: x['file'] == 'test/metrics/cc/Complicated.java', res['data']))
         self.assertEqual(data[0]['complexity'], 12)
 
-        data = list(filter(lambda x: x['file'] == 'test/metrics/cc/javafiles/OtherClass.java', res['data']))
+        data = list(filter(lambda x: x['file'] == 'test/metrics/cc/OtherClass.java', res['data']))
         self.assertEqual(data[0]['complexity'], 3)
 
-        errors = list(filter(lambda x: x['file'] == 'test/metrics/cc/javafiles/ooo.java', res['errors']))
+        errors = list(filter(lambda x: x['file'] == 'test/metrics/cc/ooo.java', res['errors']))
         self.assertEqual(errors[0]['message'][0:12], 'PMDException')
 
-        file = 'test/metrics/cc/javafiles/Complicated.java'
+        file = 'test/metrics/cc/Complicated.java'
         metric = CCMetric(file)
         res = metric.value(True)
         self.assertEqual(res['data'][0]['complexity'], 12)
         self.assertEqual(res['data'][0]['file'], file)
 
-        file = 'test/metrics/cc/javafiles/ooo.java'
+        file = 'test/metrics/cc/ooo.java'
         metric = CCMetric(file)
         res = metric.value(True)
         self.assertEqual(res['errors'][0]['message'][0:12], 'PMDException')
         self.assertEqual(res['errors'][0]['file'], file)
 
         with self.assertRaises(Exception) as context:
-            file = 'test/metrics/cc/javafiles/ooo1.java'
+            file = 'test/metrics/cc/ooo1.java'
             metric = CCMetric(file)
             res = metric.value(True)
-        self.assertTrue('File test/metrics/cc/javafiles/ooo1.java does not exist' == str(context.exception))
+        self.assertTrue('File test/metrics/cc/ooo1.java does not exist' == str(context.exception))
 
-        file = 'test/metrics/cc/javafiles/OtherClass.java'
+        file = 'test/metrics/cc/OtherClass.java'
         metric = CCMetric(file)
         res = metric.value(True)
         self.assertEqual(res['data'][0]['complexity'], 3)
