0ce5f0116ce8e0e85a986f4a84d4b2c58c339dfe
Evgeny Maslov <e00533045@china.huawei.com>
2020-02-04 17:07:10 +0300

    Write tests

---
+++ test/patterns/for_nested/NestedForInNestedClasses.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+class Test {
+
+    class TestInside {
+        public void start() {
+            ArrayList<Boolean> list = new ArrayList<Boolean>();
+            for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 10; i++)
+                    list.add(Boolean.FALSE);
+        }
+    }
+
+    TestInside a = new TestInside();
+
+    public void start() {
+        final JndiService jndiService = serviceRegistry
+                .getService(JndiService.class);
+        final ConnectionFactory jmsConnectionFactory = jndiService
+                .locate(jmsConnectionFactoryName);
+
+        this.jmsConnection = jmsConnectionFactory.createConnection();
+        this.jmsSession = jmsConnection.createSession(
+                true,
+                Session.AUTO_ACKNOWLEDGE
+        );
+        ArrayList<Boolean> list = new ArrayList<Boolean>();
+        for (int i = 0; i < 10; i++)
+            list.add(Boolean.FALSE);
+        ArrayList<Boolean> list = new ArrayList<Boolean>();
+        for (int i = 0; i < 10; i++)
+            list.add(Boolean.FALSE);
+
+        this.publisher = jmsSession.createProducer(destination);
+    }
+
+    public void foo() {
+        a.start();
+    }
+}
