94f08fa9478920cd19c2c57c12721a263cf8fc1c
ALEXEY ZORCHENKOV <zorchenkov.alexey@huawei.com>
2020-02-04 12:07:05 +0000

    Task #7 Implement download java repositories script

---
+++ aibolit/scripts/trending.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+import os
+import uuid
+import shutil
+import requests as r
+from bs4 import BeautifulSoup
+
+
+if not os.path.isdir("output"):
+    os.mkdir('output')
+r = r.get('https://github.com/trending/java?since=daily')
+soup = BeautifulSoup(r.text)
+for city in soup.find_all('h1', {'class' : 'h3 lh-condensed'}):
+    path = city.a['href'].split('/')
+    if not os.path.isdir(os.path.join('output', path[len(path) - 1])):
+        result = subprocess.run(['git', 'clone', 'https://github.com' + city.a['href'] + '.git'], cwd='output')
+    for root, dirs, files in os.walk(os.path.join('output', path[len(path) - 1])):
+        for file in files:
+            if file[-5:] == ".java":
+                count = 0
+                with open(os.path.join(root, file), 'r') as f:
+                    for line in f:
+                        count += 1
+                    if count > 50 and count < 300:
+                        print(os.path.join(root, file))
