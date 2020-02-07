29231990784f8e8e44b86b959e9826cea676e146
ALEXEY ZORCHENKOV <zorchenkov.alexey@huawei.com>
2020-02-07 13:38:55 +0000

    Task #7 Enhance got repositories download script

--- scripts/01-fetch-github.py
+++ scripts/01-fetch-github.py
@@ -41,15 +41,3 @@ for city in soup.find_all('h1', {'class': 'h3 lh-condensed'}):
     if not os.path.isdir(os.path.join(repos, path[len(path) - 2], path[len(path) - 1])):
         result = subprocess.run(['git', 'clone', 'https://github.com' + city.a['href'] + '.git'],
                                 cwd=os.path.join(repos, path[len(path) - 2]))
-    for root, dirs, files in os.walk(os.path.join(repos, path[len(path) - 2], path[len(path) - 1])):
-        for file in files:
-            if file[-5:] == ".java":
-                count = 0
-                with open(os.path.join(root, file), 'r') as f:
-                    for line in f:
-                        count += 1
-                    if count > 50 and count < 300:
-                        s = str(os.path.join(root, file))
-                        f = open(txt, 'a')
-                        f.write(s + '\n')
-                        f.close()
