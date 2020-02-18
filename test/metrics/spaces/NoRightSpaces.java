a07fa3c1bc24af73b3ce92547631a33fef000bdd
Evgeny Maslov <e00533045@china.huawei.com>
2020-02-18 12:01:31 +0300

    SpacesCounter added

---
+++ test/metrics/spaces/NoRightSpaces.java
@@ -0,0 +1,12 @@
+class Test                                                                   {
+                                                         public void start() {
+final JndiService jndiService = serviceRegistry.getService(JndiService.class);
+final ConnectionFactory jmsConnectionFactory = jndiService.locate(aaaaaaaaaa);
+
+                 this.jmsConnection = jmsConnectionFactory.createConnection();
+this.jmsSession = jmsConnection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+          final Destination destination = jndiService.locate(destinationName);
+
+                      this.publisher = jmsSession.createProducer(destination);
+                                                                             }
+                                                                             }
