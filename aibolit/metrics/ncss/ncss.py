dc2782b0d4d37d56374a0f7c87a3aefb376b95e9
andrey gusev <44449418+angusev@users.noreply.github.com>
2020-03-17 13:34:21 +0300

    add ncss.py

---
+++ aibolit/metrics/ncss/ncss.py
@@ -0,0 +1,37 @@
+import javalang
+
+
+class NCSSMetric():
+    filename = ''
+
+    def __init__(self, input):
+        """Initialize class."""
+        if len(filename) == 0:
+            raise ValueError('Empty file for analysis')
+        self.filename = filename
+    
+    def value(self):
+        f = open(self.filename, "r")
+        code = f.read()
+        f.close()
+
+        tokens = list(javalang.tokenizer.tokenize(code))
+        
+        special = ['Statement']
+        
+        tree = javalang.parse.parse(code)
+        
+        metric = 0
+        for path, node in tree:
+            node_type = str(type(node))
+            if 'Statement' in node_type:
+                metric += 1
+            elif 'VariableDeclarator' == node_type:
+                metric += 1
+            elif 'Assignment' == node_type:
+                metric += 1
+            elif 'Declaration' in node_type and 'LocalVariableDeclaration' not in node_type \
+                                            and 'PackageDeclaration' not in node_type:
+                metric += 1
+            
+        return metric
