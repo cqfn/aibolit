1241dc903c2a5dbf947178d7f1520f2e30e9db03
silverCase <serebrennikov.oleg.p@gmail.com>
2020-03-19 19:41:10 +0300

    New "er class" pattern; tests for "er class" pattern

---
+++ aibolit/patterns/er_class/er_class.py
@@ -0,0 +1,35 @@
+import javalang
+
+
+class ErClass:
+
+    def __init__(self):
+        pass
+
+    def __file_to_ast(self, filename: str) -> javalang.ast.Node:
+        """
+        Takes path to java class file and returns AST Tree
+        :param filename:
+        :return: Tree
+        """
+        with open(filename, encoding='utf-8') as file:
+            tree = javalang.parse.parse(file.read())
+        return tree
+
+    def value(self, filename: str):
+        classes = ( 'manager',
+                    'controller',
+                    'router',
+                    'dispatcher',
+                    'printer',
+                    'writer',
+                    'reader',
+                    'parser',
+                    'generator',
+                    'renderer',
+                    'listener',
+                    'producer',
+                    'holder',
+                    'interceptor')
+        tree = self.__file_to_ast(filename).filter(javalang.tree.ClassDeclaration)
+        return [node._position.line for _, node in tree if [name for name in classes if name in node.name.lower()] != [] ]
