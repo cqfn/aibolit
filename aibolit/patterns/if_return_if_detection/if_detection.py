49bd1f2398eb70c9f03f533eef6267b672297b18
Vitaly-Protasov <izakladno@yandex.ru>
2020-03-24 11:17:36 +0300

    v3

--- aibolit/patterns/if_return_if_detection/if_detection.py
+++ aibolit/patterns/if_return_if_detection/if_detection.py
@@ -1,5 +1,5 @@
 import javalang
-
+from aibolit.var_middle.var_middle import ASTNode, JavalangImproved
 
 # mapping between javalang node class names and Java keywords
 NODE_KEYWORD_MAP = {
@@ -11,48 +11,11 @@ NODE_KEYWORD_MAP = {
 }
 
 
-class ASTNode:
-
-    def __init__(self, line, method_line, node):
-        self.line = line  # node line number in the file
-        self.method_line = method_line  # line number where parent method declared
-        self.node = node  # javalang AST node object
-
-
-class JavalangImproved:
-
+class newJavalangImproved(JavalangImproved):
     def __init__(self, filename: str):
         tree, lines = self.__file_to_ast(filename)
         self.tree = tree
         self.lines = lines
-
-    def __file_to_ast(self, filename: str):
-        '''Takes path to java class file and returns AST Tree'''
-        with open(filename, encoding='utf-8') as file:
-            tree = javalang.parse.parse(file.read())
-
-        with open(filename, encoding='utf-8') as file:
-            lines = file.readlines()
-        return tree, lines
-
-    def __find_keyword(self, lines, keyword, start):
-        for i in range(start - 1, len(lines)):
-            if keyword in lines[i]:
-                return i + 1
-        return -1
-
-    def __fix_line_number_if_possible(self, node: javalang.ast.Node, line_n):
-        node_class_name = node.__class__.__name__
-        if node_class_name not in NODE_KEYWORD_MAP:
-            return line_n
-        line = self.__find_keyword(
-            self.lines,
-            NODE_KEYWORD_MAP[node_class_name],
-            line_n
-        )
-        if line == -1:
-            return line_n
-        return line
 
     def __tree_to_nodes(self, tree, line=1, parent_method_line=None):
         '''Return AST nodes with line numbers sorted by line number'''
@@ -79,11 +42,6 @@ class JavalangImproved:
 
         return [ASTNode(line, parent_method_line, tree)] + res
 
-    def tree_to_nodes(self):
-        '''Return AST nodes as list with line numbers sorted by line number'''
-        nodes = self.__tree_to_nodes(self.tree)
-        return sorted(nodes, key=lambda v: v.line)
-
     def filter(self, ntypes):
         nodes = self.tree_to_nodes()
         array = []
@@ -106,7 +64,7 @@ class CountIfReturn:
 
     def value(self, filename: str):
         ''''''
-        tree = JavalangImproved(filename)
+        tree = newJavalangImproved(filename)
         if_decls = tree.filter([javalang.tree.IfStatement])
 
         return if_decls
