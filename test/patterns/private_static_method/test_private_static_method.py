6cb5dd37bfb839d3ac1df67eb1efe832a525eb36
paulodamaso <pauloeduardolobo@gmail.com>
2020-04-04 23:32:00 -0300

    For #127: review corrections

--- test/patterns/private_static_method/test_private_static_method.py
+++ test/patterns/private_static_method/test_private_static_method.py
@@ -8,6 +8,6 @@ from pathlib import Path
 class TestPrivateStaticMethod(TestCase):
     dir_path = Path(os.path.realpath(__file__)).parent
 
-    def test_find_non_final_atributes(self):
+    def test_find_private_static_methods(self):
         lines = PrivateStaticMethod().value(Path(self.dir_path, 'PrivateStaticMethod.java'))
-        self.assertEqual(1, len(lines))
+        self.assertEqual(2, lines[0].position.line)
