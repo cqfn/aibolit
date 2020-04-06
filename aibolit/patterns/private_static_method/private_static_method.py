625cf0598afd2fbea9ccd78e42a1ede618f6a8d6
paulodamaso <pauloeduardolobo@gmail.com>
2020-04-06 12:09:14 -0300

    For #127: review corrections

--- aibolit/patterns/private_static_method/private_static_method.py
+++ aibolit/patterns/private_static_method/private_static_method.py
@@ -9,4 +9,7 @@ class PrivateStaticMethod:
 
     def value(self, filename: str):
         tree = AST(filename).value().filter(javalang.tree.MethodDeclaration)
-        return [node for path, node in tree if all(elem in node.modifiers for elem in ['private', 'static'])]
+        private_static_methods = list()
+        for node in [node for path, node in tree if all(elem in node.modifiers for elem in ['private', 'static'])]:
+            private_static_methods.append(node.position.line)
+        return private_static_methods
--- test/patterns/private_static_method/test_private_static_method.py
+++ test/patterns/private_static_method/test_private_static_method.py
@@ -9,5 +9,4 @@ class TestPrivateStaticMethod(TestCase):
     dir_path = Path(os.path.realpath(__file__)).parent
 
     def test_find_private_static_methods(self):
-        lines = PrivateStaticMethod().value(Path(self.dir_path, 'PrivateStaticMethod.java'))
-        self.assertEqual(2, lines[0].position.line)
+        self.assertEqual([2], PrivateStaticMethod().value(Path(self.dir_path, 'PrivateStaticMethod.java')))
