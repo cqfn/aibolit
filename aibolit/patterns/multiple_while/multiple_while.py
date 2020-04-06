0acd46b4241412978f9c41928155d9f4f8733a56
paulodamaso <pauloeduardolobo@gmail.com>
2020-04-06 10:31:02 -0300

    For #120: review corrections

--- aibolit/patterns/multiple_while/multiple_while.py
+++ aibolit/patterns/multiple_while/multiple_while.py
@@ -1,6 +1,7 @@
 import javalang
 
 from collections import defaultdict
+from aibolit.utils.ast import AST
 import hashlib
 import itertools
 
@@ -9,16 +10,6 @@ class MultipleWhile:
 
     def __init__(self):
         pass
-
-    def __file_to_ast(self, filename: str) -> javalang.ast.Node:
-        """
-        Takes path to java class file and returns AST Tree
-        :param filename:
-        :return: Tree
-        """
-        with open(filename, encoding='utf-8') as file:
-            res = javalang.parse.parse(file.read())
-        return res
 
     def value(self, filename: str):
         """
@@ -29,9 +20,8 @@ class MultipleWhile:
         [[10, 'func1'], [10, 'fun2']], [[23, 'run'], [23, 'start']]]
         """
 
-        tree = self.__file_to_ast(filename)
         res = defaultdict(list)
-        for _, method_node in tree.filter(javalang.tree.MethodDeclaration):
+        for _, method_node in AST(filename).value().filter(javalang.tree.MethodDeclaration):
             for _, while_node in method_node.filter(javalang.tree.WhileStatement):
                 func_name = '{f}({params})'.format(
                     f=method_node.name,
