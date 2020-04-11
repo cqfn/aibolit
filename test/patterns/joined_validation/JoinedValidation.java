f422e5a2814a8f0261d15ea9bfc4723144f44aa7
paulodamaso <pauloeduardolobo@gmail.com>
2020-04-11 01:12:49 -0300

    For #128: joined validation pattern

---
+++ test/patterns/joined_validation/JoinedValidation.java
@@ -0,0 +1,25 @@
+class JoinedValidation {
+    void print(int x, int y) {
+        if (x == 1) { // not here!
+            throw new Exception("Oops");
+        }
+        if (x == 1 && y == 1) { // not here!
+            throw new Exception("Oops");
+        }
+        if (x == 1 || y == 1) { // here!
+            throw new Exception("Oops");
+        }
+        if (x == 1) { // not here!
+            System.out.println("Not here, also");
+            throw new Exception("Oops");
+        }
+        if (x == 1 && y == 1) { // not here!
+            System.out.println("Not here, also");
+            throw new Exception("Oops");
+        }
+        if (x == 1 || y == 1) { // here!
+            System.out.println("Not here, also");
+            throw new Exception("Oops");
+        }
+    }
+}
