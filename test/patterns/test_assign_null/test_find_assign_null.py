dfe46f6629f81a5fc2b19e9c4118d489e4f0c164
lukyanoffpashok <lukyanoffpashok@yandex.ru>
2020-04-14 17:18:56 +0300

    add assign null pattern

--- test/patterns/test_assign_null/test_find_assign_null.py
+++ test/patterns/test_assign_null/test_find_assign_null.py
@@ -22,20 +22,20 @@
 
 import os
 from unittest import TestCase
-from aibolit.patterns.assign_null_finder.assign_null_finder import AssignNullFinder
+from aibolit.patterns.assign_null_finder.assign_null_finder import NullAssignment
 
 
-class TestFindAssignNull(TestCase):
+class TestNullAssignment(TestCase):
     cur_dir = os.path.dirname(os.path.realpath(__file__))
-    pattern = AssignNullFinder()
 
     def test_several(self):
-        lines = self.pattern.value(self.cur_dir + '/several.java')
-        assert lines == [5, 6, 11, 15, 21, 22]
+        lines = NullAssignment().value(self.cur_dir + '/several.java')
+        self.assertEqual(lines, [5, 6, 11, 15, 21, 22])
 
     def test_one(self):
-        lines = self.pattern.value(self.cur_dir + '/one.java')
-        assert lines == [8]
-    def test_one(self):
-        lines = self.pattern.value(self.cur_dir + '/not_null.java')
-        assert lines == []
+        lines = NullAssignment().value(self.cur_dir + '/one.java')
+        self.assertEqual(lines, [8])
+
+    def test_not_null(self):
+        lines = NullAssignment().value(self.cur_dir + '/not_null.java')
+        self.assertEqual(lines, [])
