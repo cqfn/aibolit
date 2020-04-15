85f0f95ea58c8be4a9e97c4512172e9807f5e84c
Vitaly-Protasov <you@example.com>
2020-04-15 18:44:27 +0300

    fixed comments

--- aibolit/metrics/cognitiveC/cognitive_c.py
+++ aibolit/metrics/cognitiveC/cognitive_c.py
@@ -32,7 +32,11 @@ class CognitiveComplexity:
     def __init__(self):
         self.complexity = 0
 
-    def get_complexity(self, childs, nested_level=0):
+    def traverse_childs(self, block: javalang.tree, nested_level: int):
+        for each_child in block.children:
+            self.get_complexity(each_child, nested_level)
+
+    def get_complexity(self, childs, nested_level: int):
         child_arr = childs if isinstance(childs, List) else [childs]
 
         for c in child_arr:
@@ -40,26 +44,23 @@ class CognitiveComplexity:
             if hasattr(c, 'children'):
                 if type(c) in increment_for and type(c) in nested_for:
                     self.complexity += (1 + nested_level)
-                    for each_child in c.children:
-                        self.get_complexity(each_child, nested_level + 1)
+                    self.traverse_childs(c, nested_level + 1)
 
                 elif type(c) in increment_for and type(c) not in nested_for:
                     self.complexity += 1
-                    for each_child in c.children:
-                        self.get_complexity(each_child, nested_level)
+                    self.traverse_childs(c, nested_level)
                 else:
-                    for each_child in c.children:
-                        self.get_complexity(each_child, nested_level)
+                    self.traverse_childs(c, nested_level)
 
             continue
 
-    def value(self, filename: str):
+    def value(self, filename: str) -> int:
 
         nodes = JavalangImproved(filename).tree_to_nodes()
 
         for i in nodes:
             if type(i.node) in [javalang.tree.MethodDeclaration]:
-                self.get_complexity(i.node)
+                self.get_complexity(i.node, 0)
 
         final_value, self.complexity = self.complexity, 0
         return final_value
