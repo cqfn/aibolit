51339d25ce4225db79704b0ae2050eaefa304a82
silverCase <serebrennikov.oleg.p@gmail.com>
2020-04-15 14:35:23 +0300

    Add detection for ternary expression with null value

--- aibolit/patterns/send_null/send_null.py
+++ aibolit/patterns/send_null/send_null.py
@@ -12,7 +12,15 @@ class SendNull:
         lst: List[int] = []
         tree = AST(filename).value()
         invocation_tree = tree.filter(javalang.tree.Invocation)
-        creator_tree = tree.filter(javalang.tree.Creator)
+        arg_list = [x for _, x in invocation_tree]
+
+        for argument in arg_list:
+            ternary_list = argument.filter(javalang.tree.TernaryExpression)
+            for path, expr in ternary_list:
+                if isinstance(expr.if_false, javalang.tree.Literal) and expr.if_false.value == 'null':
+                    lst.append(argument._position.line)
+                if isinstance(expr.if_true, javalang.tree.Literal) and expr.if_true.value == 'null':
+                    lst.append(argument._position.line)
 
         for path, node in tree:
             try:
