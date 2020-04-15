d992bbb8b133abd1001c09cd68a7be5b0a8fa07d
silverCase <serebrennikov.oleg.p@gmail.com>
2020-04-15 07:58:13 +0300

    Constructors with "null" parameter  are also recognized as a pattern

--- aibolit/patterns/send_null/send_null.py
+++ aibolit/patterns/send_null/send_null.py
@@ -12,11 +12,15 @@ class SendNull:
         lst: List[int] = []
         tree = AST(filename).value()
         invocation_tree = tree.filter(javalang.tree.Invocation)
+        creator_tree = tree.filter(javalang.tree.Creator)
 
-        for path, node in invocation_tree:
-            for argument in node.arguments:
-                if isinstance(argument, javalang.tree.Literal) and argument.value == "null" and \
-                        argument._position.line not in lst:
-                    lst.append(argument._position.line)
+        for path, node in tree:
+            try:
+                for argument in node.arguments:
+                    if isinstance(argument, javalang.tree.Literal) and argument.value == "null" and \
+                            argument._position.line not in lst:
+                        lst.append(argument._position.line)
+            except (AttributeError, TypeError):
+                pass
         lst = sorted(lst)
         return lst
