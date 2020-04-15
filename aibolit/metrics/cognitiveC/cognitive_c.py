8dc6e794a1bcd2258b9930891359ba5ab285f641
Vitaly-Protasov <you@example.com>
2020-04-15 13:57:10 +0300

    added nesting and other incrementing properties

--- aibolit/metrics/cognitiveC/cognitive_c.py
+++ aibolit/metrics/cognitiveC/cognitive_c.py
@@ -32,7 +32,7 @@ class CognitiveComplexity:
     def __init__(self):
         self.complexity = 0
 
-    def test(self, childs, nested_level=0):
+    def get_complexity(self, childs, nested_level=0):
         child_arr = childs if isinstance(childs, List) else [childs]
 
         for c in child_arr:
@@ -41,15 +41,15 @@ class CognitiveComplexity:
                 if type(c) in increment_for and type(c) in nested_for:
                     self.complexity += (1 + nested_level)
                     for each_child in c.children:
-                        self.test(each_child, nested_level + 1)
+                        self.get_complexity(each_child, nested_level + 1)
 
                 elif type(c) in increment_for and type(c) not in nested_for:
                     self.complexity += 1
                     for each_child in c.children:
-                        self.test(each_child, nested_level)
+                        self.get_complexity(each_child, nested_level)
                 else:
                     for each_child in c.children:
-                        self.test(each_child, nested_level)
+                        self.get_complexity(each_child, nested_level)
 
             continue
 
@@ -59,8 +59,7 @@ class CognitiveComplexity:
 
         for i in nodes:
             if type(i.node) in [javalang.tree.MethodDeclaration]:
-                self.test(i.node)
+                self.get_complexity(i.node)
 
-        final_value = self.complexity
-        self.complexity = 0
+        final_value, self.complexity = self.complexity, 0
         return final_value
