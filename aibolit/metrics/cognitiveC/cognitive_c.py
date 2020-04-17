32760801bf5334bb035a9df0dca27a2a1181f8cd
Vitaly-Protasov <you@example.com>
2020-04-17 09:43:11 +0300

    improved syntax

--- aibolit/metrics/cognitiveC/cognitive_c.py
+++ aibolit/metrics/cognitiveC/cognitive_c.py
@@ -1,30 +1,32 @@
-import javalang
+from javalang.tree import IfStatement, SwitchStatement, ForStatement, WhileStatement
+from javalang.tree import DoStatement, CatchClause, BreakStatement, ContinueStatement
+from javalang.tree import TernaryExpression, BinaryOperation, MethodDeclaration
 from aibolit.utils.ast import AST
 from typing import List, Any, Type
 
 increment_for: List[Type] = [
-    javalang.tree.IfStatement,
-    javalang.tree.SwitchStatement,
-    javalang.tree.ForStatement,
-    javalang.tree.WhileStatement,
-    javalang.tree.DoStatement,
-    javalang.tree.CatchClause,
-    javalang.tree.BreakStatement,
-    javalang.tree.ContinueStatement,
-    javalang.tree.TernaryExpression,
-    javalang.tree.BinaryOperation,
+    IfStatement,
+    SwitchStatement,
+    ForStatement,
+    WhileStatement,
+    DoStatement,
+    CatchClause,
+    BreakStatement,
+    ContinueStatement,
+    TernaryExpression,
+    BinaryOperation,
 ]
 
 nested_for: List[Type] = [
-    javalang.tree.IfStatement,
-    javalang.tree.SwitchStatement,
-    javalang.tree.ForStatement,
-    javalang.tree.WhileStatement,
-    javalang.tree.DoStatement,
-    javalang.tree.CatchClause,
+    IfStatement,
+    SwitchStatement,
+    ForStatement,
+    WhileStatement,
+    DoStatement,
+    CatchClause,
 ]
 
-logical_operators = ['&', '&&', '^', '|', '||']
+logical_operators: List[str] = ['&', '&&', '^', '|', '||']
 
 
 class CognitiveComplexity:
@@ -34,24 +36,24 @@ class CognitiveComplexity:
     def __init__(self):
         self.complexity = 0
 
-    def traverse_childs(self, block: Any, nested_level: int):
+    def traverse_childs(self, block: Any, nested_level: int) -> None:
 
         for each_child in block.children:
             self.get_complexity(each_child, nested_level)
 
-    def increment_logical_operators(self, block: Type, prev_operator: str):
+    def increment_logical_operators(self, block: Type, prev_operator: str) -> None:
         for each_block in [block.operandr, block.operandl]:
 
-            if isinstance(each_block, javalang.tree.BinaryOperation) and prev_operator in logical_operators:
+            if isinstance(each_block, BinaryOperation) and prev_operator in logical_operators:
                 if prev_operator != each_block.operator:
                     self.complexity += 1
 
-                elif isinstance(each_block.operandr, javalang.tree.BinaryOperation):
+                elif isinstance(each_block.operandr, BinaryOperation):
                     self.complexity += 1
 
                 self.increment_logical_operators(each_block, each_block.operator)
 
-    def get_complexity(self, block: Any, nested_level: int):
+    def get_complexity(self, block: Any, nested_level: int) -> None:
         block_arr = block if isinstance(block, List) else [block]
 
         for each_block in block_arr:
@@ -61,7 +63,7 @@ class CognitiveComplexity:
                     self.traverse_childs(each_block, nested_level + 1)
 
                 elif type(each_block) in increment_for and type(each_block) not in nested_for:
-                    if isinstance(each_block, javalang.tree.BinaryOperation):
+                    if isinstance(each_block, BinaryOperation):
                         if each_block.operator in logical_operators:
                             self.complexity += 1
                             self.increment_logical_operators(each_block, each_block.operator)
@@ -78,7 +80,7 @@ class CognitiveComplexity:
     def value(self, filename: str) -> int:
 
         tree = AST(filename).value()
-        for _, method in tree.filter(javalang.tree.MethodDeclaration):
+        for _, method in tree.filter(MethodDeclaration):
             self.get_complexity(method, 0)
 
         final_value, self.complexity = self.complexity, 0
