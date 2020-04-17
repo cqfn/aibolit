2043b7412c9337df00c7ee9da9deb278b5003718
Vitaly-Protasov <you@example.com>
2020-04-17 14:52:28 +0300

    added detecting recursion call

--- aibolit/metrics/cognitiveC/cognitive_c.py
+++ aibolit/metrics/cognitiveC/cognitive_c.py
@@ -1,6 +1,6 @@
 from javalang.tree import IfStatement, SwitchStatement, ForStatement, WhileStatement
 from javalang.tree import DoStatement, CatchClause, BreakStatement, ContinueStatement
-from javalang.tree import TernaryExpression, BinaryOperation, MethodDeclaration
+from javalang.tree import TernaryExpression, BinaryOperation, MethodDeclaration, MethodInvocation
 from aibolit.utils.ast import AST
 import javalang
 from typing import List, Any, Type
@@ -16,6 +16,7 @@ increment_for: List[Type] = [
     ContinueStatement,
     TernaryExpression,
     BinaryOperation,
+    MethodInvocation,
 ]
 
 nested_for: List[Type] = [
@@ -36,13 +37,14 @@ class CognitiveComplexity:
     '''
     def __init__(self):
         self.complexity = 0
+        self.method_name = None
 
     def traverse_childs(self, block: Any, nested_level: int) -> None:
 
         for each_child in block.children:
             self.get_complexity(each_child, nested_level)
 
-    def increment_logical_operators(self, block: javalang.tree.BinaryOperation, prev_operator: str) -> None:
+    def increment_logical_operators(self, block: BinaryOperation, prev_operator: str) -> None:
         for each_block in [block.operandr, block.operandl]:
 
             if isinstance(each_block, BinaryOperation) and prev_operator in logical_operators:
@@ -53,6 +55,11 @@ class CognitiveComplexity:
                     self.complexity += 1
 
                 self.increment_logical_operators(each_block, each_block.operator)
+
+    def is_recursion_call(self, block: MethodInvocation) -> bool:
+        if self.method_name == block.member:
+            return True
+        return False
 
     def get_complexity(self, block: Any, nested_level: int) -> None:
         block_arr = block if isinstance(block, List) else [block]
@@ -69,20 +76,27 @@ class CognitiveComplexity:
                             self.complexity += 1
                             self.increment_logical_operators(each_block, each_block.operator)
 
-                        continue
+                    elif isinstance(each_block, MethodInvocation):
+                        is_recursion = self.is_recursion_call(each_block)
+                        self.complexity += is_recursion
 
-                    self.complexity += 1
-                    self.traverse_childs(each_block, nested_level)
+                    else:
+                        self.complexity += 1
+                        self.traverse_childs(each_block, nested_level)
+
                 else:
                     self.traverse_childs(each_block, nested_level)
-
-            continue
 
     def value(self, filename: str) -> int:
 
         tree = AST(filename).value()
-        for _, method in tree.filter(MethodDeclaration):
-            self.get_complexity(method, 0)
+        for _, class_body in tree.filter(javalang.tree.ClassDeclaration):
+            for method in (class_body.body):
+                if isinstance(method, MethodDeclaration):
+
+                    # memorize the name for detecting recursion call
+                    self.method_name = method.name
+                    self.get_complexity(method, 0)
 
         final_value, self.complexity = self.complexity, 0
         return final_value
