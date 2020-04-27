689f82712f9d60590fa4df1c79e20d4d5e9fc664
paulodamaso <pauloeduardolobo@gmail.com>
2020-04-27 16:51:20 -0300

    For #184: counting number of method executions in local method calls pattern

--- aibolit/metrics/local_methods_calls/local_methods_calls.py
+++ aibolit/metrics/local_methods_calls/local_methods_calls.py
@@ -21,14 +21,14 @@
 # SOFTWARE.
 
 
-class LocalMethodsCalled:
+class LocalMethodsCalls:
     """
         Measure the number of local methods called by the class.
         @todo #183:30min Implement local method called metric
          In the methods in the class an local method (method that is defined in this class)
          can be called. So number of local methods calls by all the methods in the
          class is suggested as a metric. Implement this metric and then
-         enable tests in test_local_methods_called.py
+         enable tests in test_local_methods_calls.py
     """
 
     def __init__(self):
--- test/metrics/local_methods_calls/test_local_methods_calls.py
+++ test/metrics/local_methods_calls/test_local_methods_calls.py
@@ -22,17 +22,17 @@
 
 import os
 import unittest
-from aibolit.metrics.local_methods_called.local_methods_called import LocalMethodsCalled
+from aibolit.metrics.local_methods_calls.local_methods_calls import LocalMethodsCalls
 from pathlib import Path
 from unittest import TestCase
 
 
-class LocalMethodsCalledTest(TestCase):
+class LocalMethodsCallsTest(TestCase):
     dir_path = Path(os.path.realpath(__file__)).parent
 
     def test_no_local_method_calls(self):
         self.assertEqual(
-            LocalMethodsCalled().value(Path(self.dir_path, 'NoLocalMethodCalls.java')),
+            LocalMethodsCalls().value(Path(self.dir_path, 'NoLocalMethodCalls.java')),
             0,
             'Could not calculate how many local methods are called when none'
         )
@@ -40,15 +40,15 @@ class LocalMethodsCalledTest(TestCase):
     @unittest.skip('Not implemented')
     def test_local_method_calls(self):
         self.assertEqual(
-            LocalMethodsCalled().value(Path(self.dir_path, 'LocalMethodCalls.java')),
-            2,
+            LocalMethodsCalls().value(Path(self.dir_path, 'LocalMethodCalls.java')),
+            4,
             'Could not calculate how many local method are called when they exist'
         )
 
     @unittest.skip('Not implemented')
     def test_inner_class_local_method_calls(self):
         self.assertEqual(
-            LocalMethodsCalled().value(Path(self.dir_path, 'InnerClassLocalMethodCalls.java')),
+            LocalMethodsCalls().value(Path(self.dir_path, 'InnerClassLocalMethodCalls.java')),
             2,
             'Could not calculate how many local methods called when they exist in inner class'
         )
@@ -56,7 +56,7 @@ class LocalMethodsCalledTest(TestCase):
     @unittest.skip('Not implemented')
     def test_double_local_method_calls(self):
         self.assertEqual(
-            LocalMethodsCalled().value(Path(self.dir_path, 'DoubleLocalMethodCalls.java')),
-            1,
+            LocalMethodsCalls().value(Path(self.dir_path, 'DoubleLocalMethodCalls.java')),
+            2,
             'Could not calculate how many local methods called when they are called more than once'
         )
