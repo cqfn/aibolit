0944da5a4e7b9e74b56b754b991bc78719d94d64
Evgeny Maslov <lyriccoder@gmail.com>
2020-05-19 19:56:48 +0300

    Fix fir JAva improved version

--- aibolit/utils/ast.py
+++ aibolit/utils/ast.py
@@ -10,12 +10,12 @@ class AST:
 
     def __init__(self, filename: str):
         self._filename = filename
-        self.encoding = 'utf-8'
-
-    def value(self) -> CompilationUnit:
         with open(self._filename, 'rb') as file:
             bin_data = file.read()
 
         self.encoding = cchardet.detect(bin_data)['encoding']
+
+    def value(self) -> CompilationUnit:
+
         with open(self._filename, 'r', encoding=self.encoding) as file:  # type: ignore
             return parse(file.read())
