d2eb1f983036985ce060f6114b16af261930d2b3
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-06-04 21:32:53 +0300

    scope_status class for describing source code scopes while traversing ast.

---
+++ aibolit/utils/scope_status.py
@@ -0,0 +1,67 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Aibolit
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from enum import Enum, auto
+
+from typing import List, Set
+
+
+class ScopeStatusFlags(Enum):
+    ONLY_VARIABLE_DECLARATIONS = auto()
+    CURRENTLY_DECLARING_VARIABLE = auto()
+
+class ScopeStatus:
+    _default_scope_status: Set[ScopeStatusFlags] = \
+    {
+        ScopeStatusFlags.ONLY_VARIABLE_DECLARATIONS,
+    }
+
+    def __init__(self):
+        self._scope_stack: List[Set[ScopeStatusFlags]] = \
+            [ScopeStatus._default_scope_status.copy()]
+
+    def get_status(self):
+        try:
+            return self._scope_stack[-1]
+        except IndexError:
+            raise RuntimeError("No scopes registered.")
+
+    def add_flag(self, flag):
+        try:
+            self._scope_stack[-1].add(flag)
+        except IndexError:
+            raise RuntimeError("No scopes registered.")
+
+    def remove_flag(self, flag):
+        try:
+            self._scope_stack[-1].discard(flag)
+        except IndexError:
+            raise RuntimeError("No scopes registered.")
+
+    def enter_new_scope(self, new_scope_status=_default_scope_status):
+        self._scope_stack.append(new_scope_status.copy())
+
+    def leave_current_scope(self):
+        try:
+            self._scope_stack.pop()
+        except IndexError:
+            raise RuntimeError("No scopes registered.")
