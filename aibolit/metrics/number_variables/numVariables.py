1143989718ec4eca3215a5e4671551bf7ab6c22e
Vitaly-Protasov <izakladno@yandex.ru>
2020-06-09 17:40:36 +0300

    fixed typcheck

--- aibolit/metrics/number_variables/numVariables.py
+++ aibolit/metrics/number_variables/numVariables.py
@@ -14,10 +14,10 @@ class NumVars:
     def value(self, filename: str) -> int:
         tree = build_ast(filename)
         count_variables = set()
-        for _, var_body in tree.filter(VariableDeclarator):  # type: ignore
-            count_variables.add(var_body.name)  # type: ignore
+        for _, var_body in tree.filter(VariableDeclarator):
+            count_variables.add(var_body.name)
 
-        for _, var_body in tree.filter(MemberReference):  # type: ignore
-            count_variables.add(var_body.member)  # type: ignore
+        for _, var_body1 in tree.filter(MemberReference):
+            count_variables.add(var_body1.member)
 
         return len(count_variables)
