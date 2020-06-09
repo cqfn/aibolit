14890822e4df33bf06cd4877253a55e5f4e07183
Vitaly-Protasov <izakladno@yandex.ru>
2020-06-09 11:58:15 +0300

    fixed error

--- aibolit/metrics/number_variables/numVariables.py
+++ aibolit/metrics/number_variables/numVariables.py
@@ -16,10 +16,11 @@ class NumVars:
         count_variables = 0
         # for variables defined as the arguments of method
         for _, var_body in tree.filter(ConstructorDeclaration):
-            check_params = var_body.parameters
-            if len(check_params) != 0:
-                for _ in check_params:
-                    count_variables += 1
+            if hasattr(var_body, 'parameters'):
+                check_params = var_body.parameters
+                if len(check_params) != 0:
+                    for _ in check_params:
+                        count_variables += 1
         # for other unique variables
         count_variables += sum([1 for _, var_body in tree.filter(VariableDeclarator)])
         return count_variables
