8f6ff2427bd05413f9676199df5e00a34f367150
Evgeny Maslov <lyriccoder@gmail.com>
2020-06-17 13:29:35 +0300

    Fix bug

--- aibolit/patterns/send_null/send_null.py
+++ aibolit/patterns/send_null/send_null.py
@@ -9,6 +9,27 @@ class SendNull:
     def __init__(self):
         pass
 
+    def __find_position_recursively(self, node):
+        if not hasattr(node, 'children'):
+            return
+        else:
+            for i in node.children:
+                if not isinstance(i, list):
+                    if hasattr(i, '_position'):
+                        if i.position:
+                            return i._position.line
+                        else:
+                            for j in i.children:
+                                position = self.__find_position_recursively(j)
+                                if position:
+                                    return position
+                else:
+                    for j in i:
+                        position = self.__find_position_recursively(j)
+
+        return position
+
+
     def value(self, filename: str) -> List[int]:
         lst: List[int] = []
         tree = build_ast(filename)
@@ -20,9 +41,17 @@ class SendNull:
             ternary_list = argument.filter(javalang.tree.TernaryExpression)
             for _, expr in ternary_list:
                 if isinstance(expr.if_false, javalang.tree.Literal) and expr.if_false.value == 'null':
-                    lst.append(argument._position.line)
+                    if hasattr(argument, '_position'):
+                        lst.append(argument._position.line)
+                    else:
+                        position = self.__find_position_recursively(expr)
+                        lst.append(position)
                 if isinstance(expr.if_true, javalang.tree.Literal) and expr.if_true.value == 'null':
-                    lst.append(argument._position.line)
+                    if hasattr(argument, '_position'):
+                        lst.append(argument._position.line)
+                    else:
+                        position = self.__find_position_recursively(expr)
+                        lst.append(position)
 
         for _, node in tree:
             try:
