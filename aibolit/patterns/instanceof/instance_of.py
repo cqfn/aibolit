3c5123bbc57020ee19672b1aed766ac2b60e86d6
lukyanoffpashok <lukyanoffpashok@yandex.ru>
2020-06-24 18:00:39 +0300

    instanceof

--- aibolit/patterns/instanceof/instance_of.py
+++ aibolit/patterns/instanceof/instance_of.py
@@ -1,11 +1,23 @@
 from aibolit.utils.ast_builder import build_ast
 from aibolit.utils.ast import AST, ASTNodeType
-from networkx import dfs_labeled_edges  # type: ignore
 
 
 class InstanceOf:
     def __init__(self):
         pass
+
+    def get_line_number(self, node, tree):
+        cur_line = 0
+        cur_node = tree.tree.nodes[node]
+        if 'source_code_line' in cur_node:
+            cur_line = cur_node['source_code_line']
+            return cur_line
+        for child in tree.tree.succ[node]:
+            cur_node = tree.tree.nodes[child]
+            if 'source_code_line' in cur_node:
+                cur_line = cur_node['source_code_line']
+                break
+        return cur_line
 
     def value(self, filename: str):
         """
@@ -16,27 +28,22 @@ class InstanceOf:
         """
         tree = AST(build_ast(filename))
         lines = []
-        depth = 0
-        cur_line = 0
-        bin_depth = 0
-        m_depth = 0
-        for _, destination, edge_type in dfs_labeled_edges(tree.tree, tree.root):
-            if edge_type == 'forward':
-                cur_node = tree.tree.nodes[destination]
-                node_type = cur_node['type']
-                if 'source_code_line' in cur_node:
-                    cur_line = cur_node['source_code_line']
-                if node_type == ASTNodeType.BINARY_OPERATION:
-                    bin_depth = depth
-                if node_type == ASTNodeType.STRING and cur_node['string'] == 'instanceof' and depth - bin_depth == 1:
+
+        nodes = tree.nodes_by_type(ASTNodeType.BINARY_OPERATION)
+        for node in nodes:
+            cur_line = self.get_line_number(node, tree)
+            children = tree.children_with_type(node, ASTNodeType.STRING)
+            for child in children:
+                cur_node = tree.tree.nodes[child]
+                if cur_node['type'] == ASTNodeType.STRING and cur_node['string'] == 'instanceof':
                     lines.append(cur_line)
-                    bin_depth = 0
-                if node_type == ASTNodeType.METHOD_INVOCATION:
-                    m_depth = depth
-                if node_type == ASTNodeType.STRING and cur_node['string'] == 'isInstance' and depth - m_depth == 1:
+
+        nodes = tree.nodes_by_type(ASTNodeType.METHOD_INVOCATION)
+        for node in nodes:
+            cur_line = self.get_line_number(node, tree)
+            children = tree.children_with_type(node, ASTNodeType.STRING)
+            for child in children:
+                cur_node = tree.tree.nodes[child]
+                if cur_node['type'] == ASTNodeType.STRING and cur_node['string'] == 'isInstance':
                     lines.append(cur_line)
-                    m_depth = 0
-                depth += 1
-            elif edge_type == 'reverse':
-                depth -= 1
         return lines
