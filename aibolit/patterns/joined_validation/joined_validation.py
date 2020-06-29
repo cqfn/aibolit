12a3c1fac88ab0abfcb968481a8da64de511ae0b
lukyanoffpashok <lukyanoffpashok@yandex.ru>
2020-06-29 21:03:39 +0300

    add AST

--- aibolit/patterns/joined_validation/joined_validation.py
+++ aibolit/patterns/joined_validation/joined_validation.py
@@ -19,11 +19,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 from typing import List
-import sys
-from aibolit.types_decl import LineNumber
-from aibolit.utils.java_parser import JavalangImproved
-from javalang.tree import BinaryOperation, ThrowStatement, IfStatement
+from aibolit.utils.ast_builder import build_ast
+from aibolit.utils.ast import AST, ASTNodeType
 
 
 class JoinedValidation:
@@ -35,29 +34,32 @@ class JoinedValidation:
     def __init__(self):
         pass
 
-    def value(self, filename) -> List[LineNumber]:
+    def check_throw(self, node: int, tree: 'AST', lines: List[int]) -> (List[int], bool):
+        children_throw = list(tree.children_with_type(node, ASTNodeType.THROW_STATEMENT))
+        if len(children_throw) > 0:
+            lines.append(tree.get_attr(node, 'source_code_line'))
+            return lines, True
+        return lines, False
+
+    def value(self, filename: str) -> List[int]:
         """
         Returns the line number of joined validations found in file.
         """
-
-        def c1(node):
-            return type(node.node.condition) == BinaryOperation
-
-        def c2(node):
-            return node.node.condition.operator == '||'
-
-        def c3(node):
-            return type(node.node.then_statement) == ThrowStatement
-
-        def c4(node):
-            return hasattr(node.node.then_statement, "statements") and len(node.node.then_statement.statements) == 1
-
-        def c5(node):
-            return type(node.node.then_statement.statements[0]) == ThrowStatement
-
-        sys.setrecursionlimit(5000)
-        return [
-            node.line
-            for node in JavalangImproved(filename).filter([IfStatement])
-            if (c1(node) and c2(node)) and (c3(node) or (c4(node) and c5(node)))
-        ]
+        tree = AST.build_from_javalang(build_ast(filename))
+        lines: List[int] = []
+        for node in tree.nodes_by_type(ASTNodeType.IF_STATEMENT):
+            flag_or = False
+            for child in tree.all_children_with_type(node, ASTNodeType.BINARY_OPERATION):
+                operation_name = tree.get_binary_operation_name(child)
+                if operation_name == '||':
+                    flag_or = True
+            if not flag_or:
+                continue
+            lines, flag = self.check_throw(node, tree, lines)
+            if flag:
+                continue
+            child_block = list(tree.children_with_type(node, ASTNodeType.BLOCK_STATEMENT))
+            if len(child_block) == 0:
+                continue
+            lines, _ = self.check_throw(child_block[0], tree, lines)
+        return lines
