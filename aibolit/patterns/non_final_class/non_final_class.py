0d89b697f6ad58e8460c02253798f1d61e54eb56
andrey gusev <44449418+angusev@users.noreply.github.com>
2020-07-01 13:01:26 +0300

    refactor

--- aibolit/patterns/non_final_class/non_final_class.py
+++ aibolit/patterns/non_final_class/non_final_class.py
@@ -3,6 +3,7 @@ from javalang.tree import ClassDeclaration
 
 from aibolit.types_decl import LineNumber
 from aibolit.utils.ast_builder import build_ast
+from aibolit.utils.ast import AST, ASTNodeType
 
 
 class NonFinalClass:
@@ -10,10 +11,11 @@ class NonFinalClass:
     def __init__(self):
         pass
 
-    def value(self, filename: str) -> List[LineNumber]:
-        tree = build_ast(filename)
-        classes = tree.filter(ClassDeclaration)
+    def value(self, filename: str) -> List[LineNumber]:        
+        tree = AST.build_from_javalang(build_ast(filename))
+        nodes = tree.nodes_by_type(ASTNodeType.CLASS_DECLARATION)
+        
         return [
-            node.position.line for _, node in classes
+            node.position.line for node in nodes
             if len([v for v in ['final', 'abstract'] if v in node.modifiers]) == 0
         ]
