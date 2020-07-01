99539f315272828a650b071161761e213f74b1fc
andrey gusev <44449418+angusev@users.noreply.github.com>
2020-07-01 13:37:50 +0300

    fix

--- aibolit/patterns/non_final_class/non_final_class.py
+++ aibolit/patterns/non_final_class/non_final_class.py
@@ -14,7 +14,12 @@ class NonFinalClass:
         tree = AST.build_from_javalang(build_ast(filename))
         nodes = tree.nodes_by_type(ASTNodeType.CLASS_DECLARATION)
 
-        return [
-            node.position.line for node in nodes
-            if len([v for v in ['final', 'abstract'] if v in node.modifiers]) == 0
-        ]
+        positions = []
+        for node in nodes:
+            line = tree.get_attr(node, 'line')
+            modifiers = tree.get_attr(node, 'modifiers')
+
+            if len([v for v in ['final', 'abstract'] if v in modifiers]) == 0:
+                positions.append(line)
+
+        return positions
