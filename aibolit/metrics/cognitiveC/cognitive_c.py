c4aa2b2b10eeab632928f0cade6c0542e0ba5ff6
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-07-03 19:31:30 +0300

    Calculate cognitive complexity of logical expressions.

--- aibolit/metrics/cognitiveC/cognitive_c.py
+++ aibolit/metrics/cognitiveC/cognitive_c.py
@@ -1,8 +1,10 @@
+from itertools import groupby
+
 from aibolit.utils.ast import AST, ASTNodeType
 from aibolit.utils.java_package import JavaPackage
-from typing import List, Any, Type
+from typing import List, Any
 
-increment_for: List[Type] = [
+increment_for: List[ASTNodeType] = [
     ASTNodeType.IF_STATEMENT,
     ASTNodeType.SWITCH_STATEMENT,
     ASTNodeType.FOR_STATEMENT,
@@ -16,7 +18,7 @@ increment_for: List[Type] = [
     ASTNodeType.METHOD_INVOCATION,
 ]
 
-nested_for: List[Type] = [
+nested_for: List[ASTNodeType] = [
     ASTNodeType.IF_STATEMENT,
     ASTNodeType.SWITCH_STATEMENT,
     ASTNodeType.FOR_STATEMENT,
@@ -25,7 +27,7 @@ nested_for: List[Type] = [
     ASTNodeType.CATCH_CLAUSE,
 ]
 
-logical_operators: List[str] = ['!', '&', '&&', '^', '|', '||']
+logical_operators = ['&&', '||']
 
 
 class CognitiveComplexity:
@@ -38,7 +40,7 @@ class CognitiveComplexity:
         for each_child in ast.tree.succ[node]:
             #print(ast.get_type(node), ast.get_type(each_child))
             self._get_complexity(ast, each_child, nested_level)
-    
+
     def _check_if_statement(self, ast, expr, nested_level: int) -> None:
         '''function to work with IfStatement block'''
         bin_statement = list(ast.children_with_type(expr, ASTNodeType.BINARY_OPERATION))
@@ -51,26 +53,27 @@ class CognitiveComplexity:
         for i in elif_statements:
             self.complexity += 1
             self._check_if_statement(ast, i, nested_level)
-        
+
         for j in nested_statements:
             self.complexity += nested_level + 1
             self._get_complexity(ast, j, nested_level + 1)
-            
 
-    def _increment_logical_operators(self, block: BinaryOperation, prev_operator: str) -> None:
-        print('_increment_logical_operators')
-        return
-        for each_block in [block.operandr, block.operandl]:
+    def _increment_logical_operators(self, ast: AST, binary_operation_node: int) -> None:
+        logical_operators_sequence = self._create_logical_operators_sequence(ast, binary_operation_node)
+        self.complexity += len(list(groupby(logical_operators_sequence)))
 
-            if isinstance(each_block, BinaryOperation) and prev_operator in logical_operators:
-                if prev_operator != each_block.operator:
-                    self.complexity += 1
+    def _create_logical_operators_sequence(self, ast: AST, binary_operation_node: int) -> List[str]:
+        if ast.get_type(binary_operation_node) != ASTNodeType.BINARY_OPERATION:
+            return []
 
-                elif isinstance(each_block.operandr, BinaryOperation):
-                    self.complexity += 1
+        operator, left_side_node, right_side_node = ast.get_binary_operation_params(binary_operation_node)
+        if operator not in logical_operators:
+            return []
 
-                self._increment_logical_operators(ast, each_block, each_block.operator)
-    
+        left_sequence = self._create_logical_operators_sequence(ast, left_side_node)
+        right_sequence = self._create_logical_operators_sequence(ast, right_side_node)
+        return left_sequence + [operator] + right_sequence
+
     def _is_recursion_call(self, ast, node) -> bool:
         if ast.get_type(node) == ASTNodeType.METHOD_INVOCATION:
             if self.method_name == self._get_node_name(ast, node):
@@ -88,10 +91,10 @@ class CognitiveComplexity:
         for each_block in ast.tree.succ[node]:
             each_block_name = self._get_node_name(ast, each_block)
             each_block_type = ast.get_type(each_block)
-            
+
             if each_block_type == ASTNodeType.METHOD_DECLARATION and each_block_name != self.method_name:
                 self._nested_methods(ast, each_block, nested_level)
-            
+
             elif each_block_type == ASTNodeType.IF_STATEMENT:
                 self.complexity += 1
                 self._get_complexity(ast, each_block, nested_level + 1)
@@ -99,13 +102,13 @@ class CognitiveComplexity:
             elif each_block_type in increment_for and each_block_type in nested_for:
                 self.complexity += 1 + nested_level
                 self._get_complexity(ast, each_block, nested_level + 1)
-            
+
             elif each_block_type in increment_for and each_block_type not in nested_for:
                 if ast.get_type(each_block) == ASTNodeType.BINARY_OPERATION:
                     bin_operator = ast.get_binary_operation_name(each_block)
                     if bin_operator in logical_operators:
                         self.complexity += 1
-                        self._increment_logical_operators(ast, each_block, bin_operator)
+                        self._increment_logical_operators(ast, each_block)
                 elif each_block_type == ASTNodeType.METHOD_INVOCATION:
                     is_recursion = self._is_recursion_call(ast, each_block)
                     self.complexity += is_recursion
@@ -116,8 +119,7 @@ class CognitiveComplexity:
 
             else:
                 self._get_complexity(ast, each_block, nested_level)
-            
-    
+
     def _get_node_name(self, ast, node) -> str:
         extracted_name = None
         names = list(ast.children_with_type(node, ASTNodeType.STRING))
@@ -126,7 +128,7 @@ class CognitiveComplexity:
             if not method_name.startswith('/') and method_name not in ['', None, []]:
                 extracted_name = method_name
                 return extracted_name
-        
+
     def value(self, filename: str) -> int:
         p = JavaPackage(filename)
         for class_name in p.java_classes:
@@ -137,8 +139,9 @@ class CognitiveComplexity:
                 self.method_name = self._get_node_name(ast_each_method, ast_each_method.root)
                 self._get_complexity(ast_each_method, class_method[0], 0)
                 print(self.method_name)
-        
+
         return self.complexity
+
 
 filename = '../test.java'
 CognitiveComplexity().value(filename)
