5ed99260ba3835d7a48d6638cd45b6c264dbfcc3
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-07-03 11:55:14 +0300

    Check only top level class declarations.

--- aibolit/patterns/non_final_class/non_final_class.py
+++ aibolit/patterns/non_final_class/non_final_class.py
@@ -13,14 +13,15 @@ class NonFinalClass:
     def value(self, filename: str) -> List[LineNumber]:
         tree = AST.build_from_javalang(build_ast(filename))
         positions = []
-        for class_declaration in tree.nodes_by_type(ASTNodeType.CLASS_DECLARATION):
-            modifiers_node, = tree.get_first_n_children_with_type(class_declaration, ASTNodeType.COLLECTION, 1)
+        for class_subgraph in tree.subtrees_with_root_type(ASTNodeType.CLASS_DECLARATION):
+            class_declaration_root = class_subgraph[0]
+            modifiers_node, = tree.get_first_n_children_with_type(class_declaration_root, ASTNodeType.COLLECTION, 1)
             if modifiers_node is None:
                 continue
             modifiers = {tree.get_attr(child, 'string') for child in
                          tree.children_with_type(modifiers_node, ASTNodeType.STRING)}
             if len(modifiers & NonFinalClass._allowed_class_modifiers) == 0:
-                positions.append(tree.get_attr(class_declaration, 'source_code_line'))
+                positions.append(tree.get_attr(class_declaration_root, 'source_code_line'))
 
         return positions
 
