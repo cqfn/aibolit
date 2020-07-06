b4e3993e829b98638bb41375f169e1fc7a02a4f8
Vitaly Protasov <izakladno@yandex.ru>
2020-07-06 19:13:00 +0300

    refactored supermethod pattern and fixed errors

--- aibolit/patterns/supermethod/supermethod.py
+++ aibolit/patterns/supermethod/supermethod.py
@@ -1,7 +1,8 @@
-from javalang.parse import parse
-from javalang.tree import MethodDeclaration, SuperMethodInvocation
-
+from aibolit.utils.ast import AST, ASTNodeType
+from aibolit.utils.java_package import JavaPackage
 from aibolit.utils.encoding_detector import read_text_with_autodetected_encoding
+import re
+from typing import Set
 
 
 class SuperMethod:
@@ -9,7 +10,21 @@ class SuperMethod:
     def __init__(self):
         pass
 
-    def value(self, filename: str):
+    def _get_node_name(self, ast: AST, node: int) -> str:
+        extracted_name = None
+        names = ast.children_with_type(node, ASTNodeType.STRING)
+        for each_string in names:
+            method_name = ast.get_attr(each_string, 'string')
+            # Checking not to start with '/' is aimed to get
+            # rid of comments, which are all childs of node.
+            # We check the occurance any letter in name in order
+            # to get rid of '' string and None.
+            if not method_name.startswith('/') and re.search(r'[^\W\d]', method_name) is not None:
+                extracted_name = method_name
+                return extracted_name
+        return ''
+
+    def value(self, filename: str) -> Set[int]:
         """
         Iterates over functions and finds super.func() calls.
         Javalang doesn't have code line for super.func() call,
@@ -20,27 +35,16 @@ class SuperMethod:
         """
         results = []
         source_code = read_text_with_autodetected_encoding(filename)
-        tree = parse(source_code)
         lines = source_code.splitlines()
-        for _, method_decl_node in tree.filter(MethodDeclaration):
-            code_line = method_decl_node.position.line
-            for _, super_method_inv in method_decl_node.filter(SuperMethodInvocation):
+        tree = JavaPackage(filename)
+        for method_decl_node in tree.nodes_by_type(ASTNodeType.METHOD_DECLARATION):
+            code_line = tree.get_attr(method_decl_node, 'source_code_line')
+            for super_method_inv in tree.nodes_by_type(ASTNodeType.SUPER_METHOD_INVOCATION):
                 str_to_find = 'super.{method_name}('.format(
-                    method_name=super_method_inv.member).strip()
+                    method_name=self._get_node_name(tree, super_method_inv)).strip()
                 for iter, line in enumerate(lines[code_line - 1:]):
                     string_strip = line.strip().replace('\t', '')
                     if string_strip.find(str_to_find) > -1:
                         results.append(code_line + iter)
                         break
-        return results
-
-    def __traverse(self, tree, results):
-        descendants = tree.children
-        for children in descendants:
-            if isinstance(children, tuple) or isinstance(children, list):
-                for item in children:
-                    if isinstance(item, SuperMethodInvocation):
-                        results.append([item.member])
-                    else:
-                        self.__traverse(item, results)
-        return results
+        return set(results)
