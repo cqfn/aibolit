b5d7824c488f2489b33c4dba8e874827975cb679
Vitaly Protasov <izakladno@yandex.ru>
2020-07-07 16:36:58 +0300

    Refactored classic_setter pattern

--- aibolit/patterns/classic_setter/classic_setter.py
+++ aibolit/patterns/classic_setter/classic_setter.py
@@ -1,9 +1,14 @@
 from typing import List
-
-import javalang
-
+import re
 from aibolit.types_decl import LineNumber
-from aibolit.utils.ast_builder import build_ast
+from aibolit.ast_framework import AST, ASTNodeType
+from aibolit.ast_framework.java_package import JavaPackage
+
+extra_childs = [
+    ASTNodeType.STRING,
+    ASTNodeType.COLLECTION,
+    ASTNodeType.ANNOTATION,
+]
 
 
 class ClassicSetter:
@@ -11,24 +16,47 @@ class ClassicSetter:
     def __init__(self):
         pass
 
+    def _check_extra_child(self, ast: AST, node: int) -> List[int]:
+        body_nodes = []
+        for i in ast.tree.succ[node]:
+            if ast.get_type(i) not in extra_childs:
+                body_nodes.append(i)
+        return body_nodes
+
+    def _get_node_name(self, ast: AST, node: int) -> str:
+        extracted_name = None
+        names = ast.children_with_type(node, ASTNodeType.STRING)
+        for each_string in names:
+            method_name = ast.get_attr(each_string, 'string')
+            if not method_name.startswith('/') and re.search(r'[^\W\d]', method_name) is not None:
+                extracted_name = method_name
+                return extracted_name
+        return ''
+
     def value(self, filename: str) -> List[LineNumber]:
         lst: List[LineNumber] = []
-        tree = build_ast(filename).filter(javalang.tree.MethodDeclaration)
-        for _, node in tree:
-            if (node.return_type is None) and ('set' in node.name[:3]):
-                if (isinstance(node.body, list)) and len(node.body) < 2:
-                    for statement in node.body:
-                        if isinstance(statement, javalang.tree.StatementExpression):
-                            if isinstance(statement.expression, javalang.tree.Assignment):
-                                expression = statement.expression.expressionl
-                                if isinstance(expression, javalang.tree.This):
-                                    if statement.expression.type == '=':
-                                        if expression.selectors[0].member.lower() == node.name.lower()[3:]:
-                                            lst.append(node._position.line)
-                                    else:
-                                        break
+        ast = JavaPackage(filename)
+        method_decls = list(ast.nodes_by_type(ASTNodeType.METHOD_DECLARATION))
+        for node in method_decls:
+            method_name = self._get_node_name(ast, node)
+            body_nodes = self._check_extra_child(ast, node)
+
+            return_type = list(ast.children_with_type(node, ASTNodeType.BASIC_TYPE))
+            if len(return_type) == 0 and ('set' in method_name[:3]) and len(body_nodes) < 3:
+                childs_stat_expr = ast.children_with_type(node, ASTNodeType.STATEMENT_EXPRESSION)
+                for expr_child in childs_stat_expr:
+                    childs_assignment = ast.children_with_type(expr_child, ASTNodeType.ASSIGNMENT)
+                    for child_assign in childs_assignment:
+                        childs_this = list(ast.children_with_type(child_assign, ASTNodeType.THIS))
+                        if len(childs_this):
+                            for type_expr in ast.children_with_type(child_assign, ASTNodeType.STRING):
+                                if ast.get_attr(type_expr, 'string') == '=':
+                                    child_membref = ast.children_with_type(childs_this[0], ASTNodeType.MEMBER_REFERENCE)
+                                    for each_ref in child_membref:
+                                        if method_name.lower()[3:] == self._get_node_name(ast, each_ref).lower():
+                                            lst.append(ast.get_attr(node, 'source_code_line'))
                                 else:
                                     break
-                            else:
-                                break
+                        else:
+                            break
         return lst
