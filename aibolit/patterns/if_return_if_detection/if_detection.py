a8e9627c8e04b1ac20b161b0d6cac0918e8ae3e7
Vitaly Protasov <izakladno@yandex.ru>
2020-07-07 12:03:49 +0300

    finished

--- aibolit/patterns/if_return_if_detection/if_detection.py
+++ aibolit/patterns/if_return_if_detection/if_detection.py
@@ -15,11 +15,11 @@ class CountIfReturn:
         detected_lines = []
         ast = JavaPackage(filename).java_classes
         for class_name in ast:
-            tree = ast[class_name]
-            for index, if_node in enumerate(tree.nodes_by_type(ASTNodeType.IF_STATEMENT)):
-                all_childs = [i for i in tree.tree.succ[if_node]]
+            java_class = ast[class_name]
+            for index, if_node in enumerate(java_class.nodes_by_type(ASTNodeType.IF_STATEMENT)):
+                all_childs = [i for i in java_class.tree.succ[if_node]]
                 if len(all_childs) == 3:
-                    for i in tree.tree.succ[all_childs[1]]:
-                        if tree.get_type(i) == ASTNodeType.RETURN_STATEMENT:
-                            detected_lines += [tree.get_attr(if_node, 'source_code_line')]
+                    for i in java_class.tree.succ[all_childs[1]]:
+                        if java_class.get_type(i) == ASTNodeType.RETURN_STATEMENT:
+                            detected_lines += [java_class.get_attr(if_node, 'source_code_line')]
         return detected_lines
