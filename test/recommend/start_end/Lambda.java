61cc07fee0dd481626c9a691d6b8a38621dcbac3
Evgeny Maslov <lyriccoder@gmail.com>
2020-07-07 17:45:36 +0300

    Add tests

---
+++ test/recommend/start_end/Lambda.java
@@ -0,0 +1,45 @@
+package com.airbnb.lottie;
+
+import android.graphics.Bitmap;
+import androidx.annotation.Nullable;
+import androidx.annotation.RestrictTo;
+
+/**
+ * Data class describing an image asset exported by bodymovin.
+ */
+@SuppressWarnings({"aibolit.P23", "aibolit.P11"})
+public class LottieImageAsset {
+  private final int width;
+  private final int height;
+  private final String id;
+  @SuppressWarnings("aibolit.P27")
+  private final String fileName;
+  @SuppressWarnings({"aibolit.P23", "aibolit.P22"})
+  private final String dirName;
+  /** Pre-set a bitmap for this asset */
+  @SuppressWarnings({"aibolit.P23", "aibolit.P11"})
+  @Nullable private Bitmap bitmap;
+
+  @RestrictTo(RestrictTo.Scope.LIBRARY)
+  public LottieImageAsset(int width, int height, String id, String fileName, String dirName) {
+    this.width = width;
+    this.height = height;
+    this.id = id;
+    this.fileName = fileName;
+    this.dirName = dirName;
+  }
+
+	public ArrayList<Long> filterWithOptionalEmpty(Long threshold) {
+		Function<Long, Long> filter = last -> {
+			final Optional<Long> size;
+			if (last >= threshold) {
+				size = Optional.empty();
+			} else {
+				size = Optional.of(last);
+			}
+			return size;
+		};
+
+		return array.map(filter);
+	}
+}
