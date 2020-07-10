0bf80ebcf5d6b16eca50a52f171de890a0ae2d37
Vitaly Protasov <izakladno@yandex.ru>
2020-07-10 12:25:57 +0300

    rewrited searching code line

--- aibolit/patterns/supermethod/supermethod.py
+++ aibolit/patterns/supermethod/supermethod.py
@@ -9,20 +9,6 @@ class SuperMethod:
     def __init__(self):
         pass
 
-    def _get_node_name(self, ast: AST, node: int) -> str:
-        extracted_name = None
-        names = ast.children_with_type(node, ASTNodeType.STRING)
-        for each_string in names:
-            method_name = ast.get_attr(each_string, 'string')
-            # Checking not to start with '/' is aimed to get
-            # rid of comments, which are all childs of node.
-            # We check the occurance any letter in name in order
-            # to get rid of '' string and None.
-            if not method_name.startswith('/') and re.search(r'[^\W\d]', method_name) is not None:
-                extracted_name = method_name
-                return extracted_name
-        return ''
-
     def value(self, filename: str) -> List[int]:
         """
         Iterates over functions and finds super.func() calls.
