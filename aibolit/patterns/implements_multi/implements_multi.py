c5c6a25167e2d145c418927df5675302e8470304
Vitaly Protasov <izakladno@yandex.ru>
2020-07-14 18:52:48 +0300

    refactored implements_multi pattern

--- aibolit/patterns/implements_multi/implements_multi.py
+++ aibolit/patterns/implements_multi/implements_multi.py
@@ -1,5 +1,6 @@
-import javalang
+from typing import List
 
+from aibolit.ast_framework.ast import AST, ASTNodeType
 from aibolit.utils.ast_builder import build_ast
 
 
@@ -9,5 +10,9 @@ class ImplementsMultiFinder:
         pass
 
     def value(self, filename: str):
-        tree = build_ast(filename).filter(javalang.tree.ClassDeclaration)
-        return [node._position.line for _, node in tree if node.implements and (len(node.implements) > 1)]
+        tree = AST.build_from_javalang(build_ast(filename))
+        lines: List[int] = []
+        for node in tree.get_proxy_nodes(ASTNodeType.CLASS_DECLARATION):
+            if node.implements and len(node.implements) > 1:
+                lines.append(node.line)
+        return lines
