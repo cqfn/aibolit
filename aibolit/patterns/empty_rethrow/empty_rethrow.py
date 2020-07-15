41dccea7b7411b87766f2c45bd3b873fdf671c39
Vitaly Protasov <izakladno@yandex.ru>
2020-07-15 12:10:29 +0300

    rewritten to the new interface

--- aibolit/patterns/empty_rethrow/empty_rethrow.py
+++ aibolit/patterns/empty_rethrow/empty_rethrow.py
@@ -1,5 +1,5 @@
-from aibolit.ast_framework import ASTNodeType
-from aibolit.ast_framework.java_package import JavaPackage
+from aibolit.ast_framework import ASTNodeType, AST
+from aibolit.utils.ast_builder import build_ast
 from typing import List, Set
 
 
@@ -10,20 +10,19 @@ class EmptyRethrow:
 
     def value(self, filename) -> List[int]:
         total_code_lines: Set = set()
-        ast = JavaPackage(filename)
-        method_decls = list(ast.get_nodes(ASTNodeType.METHOD_DECLARATION))
+        ast = AST.build_from_javalang(build_ast(filename))
+        method_decls = list(ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION))
         for method_node in method_decls:
-            for try_node in list(ast.get_nodes(ASTNodeType.TRY_STATEMENT)):
-                for throw_node in list(ast.get_nodes(ASTNodeType.THROW_STATEMENT)):
-                    field_catche = ast.get_attr(try_node, 'catches')
+            for try_node in list(ast.get_proxy_nodes(ASTNodeType.TRY_STATEMENT)):
+                for throw_node in list(ast.get_proxy_nodes(ASTNodeType.THROW_STATEMENT)):
+                    field_catche = try_node.catches
                     if field_catche:
-                        catch_classes = [ast.get_attr(ast.get_attr(x[0], 'parameter')[0], 'name') for x in field_catche]
-                        mem_ref = list(ast.tree.succ[throw_node].keys())[0]
-                        if ast.get_type(mem_ref) == ASTNodeType.CLASS_CREATOR:
+                        catch_classes = [x.parameter.name for x in field_catche]
+                        mem_ref = list(throw_node.children)[0]
+                        if mem_ref.node_type == ASTNodeType.CLASS_CREATOR:
                             continue
 
                         else:
-                            member = ast.get_attr(mem_ref, 'member')
-                            if member is not None and member in catch_classes:
-                                total_code_lines.add(ast.get_attr(mem_ref, 'line'))
+                            if hasattr(mem_ref, 'member') and mem_ref.member in catch_classes:
+                                total_code_lines.add(mem_ref.line)
         return sorted(total_code_lines)
