1c95143331e3462110220a180da9827a66e6705d
Vitaly Protasov <izakladno@yandex.ru>
2020-07-15 17:18:29 +0300

    fixed

--- aibolit/patterns/classic_setter/classic_setter.py
+++ aibolit/patterns/classic_setter/classic_setter.py
@@ -1,33 +1,27 @@
-# The MIT License (MIT)
-#
-# Copyright (c) 2020 Aibolit
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from typing import Set, List
 from aibolit.ast_framework import ASTNodeType, AST
 from aibolit.utils.ast_builder import build_ast
-
+from aibolit.ast_framework.ast_node import ASTNode
 
 class ClassicSetter:
 
-    def check_nodes(self, ast: AST, check_setter_body: List[ASTNodeType]) -> bool:
+    suitable_nodes: List[ASTNodeType] = [
+        ASTNodeType.ASSERT_STATEMENT,
+        ASTNodeType.STATEMENT_EXPRESSION,
+    ]
+
+    def _process_this_statements(self, ast: AST, node: ASTNode):
+        lines: Set[int] = set()
+        for child_this in ast.get_proxy_nodes(ASTNodeType.THIS):
+            child_membref = child_this.selectors
+            if len(child_membref):
+                mem_referenced_name = child_membref[0].member
+                source_line = node.line
+                if node.name.lower()[3:] == mem_referenced_name.lower():
+                    lines.add(source_line)
+        return lines
+
+    def _check_nodes(self, ast: AST, check_setter_body: List[ASTNode]) -> bool:
         '''
         In this function we check whether
         nodes in the function are agree with
@@ -37,12 +31,8 @@ class ClassicSetter:
         This statement is not-directed child of
         STATEMENT_EXPRESSION.
         '''
-        suitable_nodes: List[ASTNodeType] = [
-            ASTNodeType.ASSERT_STATEMENT,
-            ASTNodeType.STATEMENT_EXPRESSION,
-        ]
         for node in check_setter_body:
-            if node.node_type not in suitable_nodes:
+            if node.node_type not in self.suitable_nodes:
                 return False
         return True
 
@@ -52,12 +42,6 @@ class ClassicSetter:
         for node in ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION):
             method_name = node.name
             if node.return_type is None and \
-                method_name.startswith('set') and self.check_nodes(ast, node.body):
-                    for child_this in ast.get_proxy_nodes(ASTNodeType.THIS):
-                        child_membref = child_this.selectors
-                        if len(child_membref):
-                            mem_referenced_name = child_membref[0].member
-                            source_line = node.line
-                            if method_name.lower()[3:] == mem_referenced_name.lower():
-                                lines.add(source_line)
+                    method_name.startswith('set') and self._check_nodes(ast, node.body):
+                lines = lines.union(self._process_this_statements(ast, node))
         return sorted(list(lines))
