ddf8056c7be18b8ce317d0c8d68228f73b455e53
Vitaly Protasov <izakladno@yandex.ru>
2020-07-15 12:15:46 +0300

    rewritten to the new interface

--- aibolit/patterns/er_class/er_class.py
+++ aibolit/patterns/er_class/er_class.py
@@ -1,5 +1,5 @@
-from aibolit.ast_framework import ASTNodeType
-from aibolit.ast_framework.java_package import JavaPackage
+from aibolit.ast_framework import ASTNodeType, AST
+from aibolit.utils.ast_builder import build_ast
 from typing import List
 
 
@@ -24,14 +24,14 @@ class ErClass:
                    'producer',
                    'holder',
                    'interceptor')
-        ast = JavaPackage(filename)
-        class_decls = list(ast.get_nodes(ASTNodeType.CLASS_DECLARATION))
+        ast = AST.build_from_javalang(build_ast(filename))
+        class_decls = list(ast.get_proxy_nodes(ASTNodeType.CLASS_DECLARATION))
         for node in class_decls:
             names = []
-            class_name = ast.get_attr(node, 'name').lower()
-            for n in classes:
-                if n in class_name:
+            class_name = node.name.lower()
+            for name in classes:
+                if name in class_name:
                     names.append(class_name)
             if names:
-                lines.append(ast.get_attr(node, 'line'))
+                lines.append(node.line)
         return lines
