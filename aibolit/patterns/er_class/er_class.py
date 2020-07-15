11a47f5dbece0c4fc007785c815c35578c6a5fe7
Vitaly Protasov <izakladno@yandex.ru>
2020-07-15 14:02:27 +0300

    fixed according to comments

--- aibolit/patterns/er_class/er_class.py
+++ aibolit/patterns/er_class/er_class.py
@@ -4,34 +4,30 @@ from typing import List
 
 
 class ErClass:
-
-    def __init__(self):
-        pass
+    forbiden_words_in_class_names = (
+        'manager',
+        'controller',
+        'router',
+        'dispatcher',
+        'printer',
+        'writer',
+        'reader',
+        'parser',
+        'generator',
+        'renderer',
+        'listener',
+        'producer',
+        'holder',
+        'interceptor')
 
     def value(self, filename: str) -> List[int]:
         lines: List[int] = []
-        classes = ('manager',
-                   'controller',
-                   'router',
-                   'dispatcher',
-                   'printer',
-                   'writer',
-                   'reader',
-                   'parser',
-                   'generator',
-                   'renderer',
-                   'listener',
-                   'producer',
-                   'holder',
-                   'interceptor')
         ast = AST.build_from_javalang(build_ast(filename))
-        class_decls = list(ast.get_proxy_nodes(ASTNodeType.CLASS_DECLARATION))
-        for node in class_decls:
+        for node in list(ast.get_proxy_nodes(ASTNodeType.CLASS_DECLARATION)):
             names = []
             class_name = node.name.lower()
-            for name in classes:
-                if name in class_name:
-                    names.append(class_name)
+            if any(forbiden_word in class_name for forbiden_word in self.forbiden_words_in_class_names):
+                names.append(class_name)
             if names:
                 lines.append(node.line)
         return lines
