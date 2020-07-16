1eb19847e41d02959b211eab801565eb16c07205
Vitaly Protasov <izakladno@yandex.ru>
2020-07-16 15:26:59 +0300

    final version

--- aibolit/patterns/classic_setter/classic_setter.py
+++ aibolit/patterns/classic_setter/classic_setter.py
@@ -1,4 +1,4 @@
-from typing import Set, List
+from typing import Optional, List
 from aibolit.ast_framework import ASTNodeType, AST
 from aibolit.utils.ast_builder import build_ast
 from aibolit.ast_framework.ast_node import ASTNode
@@ -15,26 +15,20 @@ class ClassicSetter:
         ASTNodeType.STATEMENT_EXPRESSION,
     ]
 
-    def _process_this_statements(self, ast: AST, node: ASTNode):
-        lines: Set[int] = set()
-        for child_this in ast.get_proxy_nodes(ASTNodeType.THIS):
+    def _process_this_statements(self, ast: AST, node: ASTNode) -> Optional[int]:
+        for child_this in ast.get_subtree(node).get_proxy_nodes(ASTNodeType.THIS):
             child_membref = child_this.selectors
             if len(child_membref):
                 mem_referenced_name = child_membref[0].member
                 source_line = node.line
-                if node.name.lower()[3:] == mem_referenced_name.lower():
-                    lines.add(source_line)
-        return lines
+                if node.name.lower() == 'set' + mem_referenced_name.lower():
+                    return source_line
+        return None
 
-    def _check_nodes(self, ast: AST, check_setter_body: List[ASTNode]) -> bool:
+    def _check_body_nodes(self, check_setter_body: List[ASTNode]) -> bool:
         '''
-        In this function we check whether
-        nodes in the function are agree with
-        the definition of classic setter or not.
-        Classic setter should consist of only several
-        assert_statement and THIS statement.
-        This statement is not-directed child of
-        STATEMENT_EXPRESSION.
+        Check whether nodes are agree with the following types
+        (in self.suitable_nodes) or not.
         '''
         for node in check_setter_body:
             if node.node_type not in self.suitable_nodes:
@@ -42,11 +36,11 @@ class ClassicSetter:
         return True
 
     def value(self, filename: str) -> List[int]:
-        lines: Set[int] = set()
+        lines: List[int] = []
         ast = AST.build_from_javalang(build_ast(filename))
         for node in ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION):
             method_name = node.name
             if node.return_type is None and \
-                    method_name.startswith('set') and self._check_nodes(ast, node.body):
-                lines = lines.union(self._process_this_statements(ast, node))
-        return sorted(list(lines))
+                    method_name.startswith('set') and self._check_body_nodes(node.body):
+                lines.append(self._process_this_statements(ast, node))
+        return sorted(filter(None, lines))
