bfe3dd96de3c6ea773ff5338603493f6168b137d
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-07-20 21:20:19 +0300

    Check return types of patterns and metrics.

--- test/integration/all.py
+++ test/integration/all.py
@@ -17,13 +17,14 @@ for filename in tqdm.tqdm(os.listdir(current_path + '/samples')):
             continue
         try:
             path_to_file = os.path.join(current_path, 'samples', filename)
-            pattern['make']().value(path_to_file)
+            patter_result = pattern['make']().value(path_to_file)
+            assert isinstance(patter_result, list) and \
+                all(isinstance(item, int) for item in patter_result), \
+                f'Pattern return {patter_result} with type {type(patter_result)}, but list of int was expected.'
         except Exception as e:
-            print(
-                f"Error in application of the pattern: {pattern['name']} \
-                    with code {pattern['code']} for file {filename}",
-                f'\nReason: {e}',
-            )
+            print(f'Error in application of the pattern "{pattern["name"]}" '
+                  f'with code {pattern["code"]} for file "{filename}"')
+            print(f'Reason: {e}')
             sys.exit(1)
 
     for metric in Config.get_patterns_config()['metrics']:
@@ -31,13 +32,14 @@ for filename in tqdm.tqdm(os.listdir(current_path + '/samples')):
             continue
         try:
             path_to_file = os.path.join(current_path, 'samples', filename)
-            metric['make']().value(path_to_file)
+            metric_result = metric['make']().value(path_to_file)
+            assert isinstance(metric_result, (int, float, np.float64)), \
+                f'Metric return {metric_result} of type {type(metric_result)}, ' \
+                'but int, float or numpy float was expected'
         except Exception as e:
-            print(
-                f"Error in application of the metric: {metric['name']} \
-                    with code {metric['code']} for file {filename}",
-                f'\nReason: {e}',
-            )
+            print(f'Error in application of the metric "{metric["name"]}" '
+                  f'with code {metric["code"]} for file "{filename}"')
+            print(f'Reason: {e}')
             sys.exit(1)
 
 sys.exit(0)
