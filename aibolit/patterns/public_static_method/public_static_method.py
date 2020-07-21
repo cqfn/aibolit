1e53bbac495b5276ddc5843f755b6992f1d97b01
Vitaly Protasov <izakladno@yandex.ru>
2020-07-21 11:13:31 +0300

    fixed

--- aibolit/patterns/public_static_method/public_static_method.py
+++ aibolit/patterns/public_static_method/public_static_method.py
@@ -1,21 +1,3 @@
-import javalang
-
-from aibolit.utils.ast_builder import build_ast
-
-
-class PublicStaticMethod:
-
-    def __init__(self):
-        pass
-
-    def value(self, filename: str):
-        tree = build_ast(filename).filter(javalang.tree.MethodDeclaration)
-        return [
-            node.position.line for
-            path, node in tree if
-            all(elem in node.modifiers for elem in ['public', 'static'])
-        ]
-
 # The MIT License (MIT)
 #
 # Copyright (c) 2020 Aibolit
