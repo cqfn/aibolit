1b554e0a561815662197aecb4abe450626d2dcb5
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-07-21 15:30:11 +0300

    Automatically register all computed fields from catalog on first access to ast_framework.

--- aibolit/ast_framework/__init__.py
+++ aibolit/ast_framework/__init__.py
@@ -1,3 +1,6 @@
 from aibolit.ast_framework.ast_node_type import ASTNodeType  # noqa: F401
 from aibolit.ast_framework.ast_node import ASTNode  # noqa: F401
 from aibolit.ast_framework.ast import AST  # noqa: F401
+
+# register all computed fields from 'computed_fields_catalog'
+import aibolit.ast_framework.computed_fields_catalog  # noqa: F401
