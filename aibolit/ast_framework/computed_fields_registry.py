2cc82b56d2137a11c34f15fcdcb666c949b1963a
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-07-22 17:43:00 +0300

    Add comments to link with SO and styling.

--- aibolit/ast_framework/computed_fields_registry.py
+++ aibolit/ast_framework/computed_fields_registry.py
@@ -30,26 +30,31 @@ if TYPE_CHECKING:
 
 class _ComputedFieldsRegistry:
     def __init__(self) -> None:
-        RegistryType = Dict['ASTNodeType', Dict[str, Callable[['ASTNode'], Any]]]
+        RegistryType = Dict["ASTNodeType", Dict[str, Callable[["ASTNode"], Any]]]
         self._registry: RegistryType = defaultdict(dict)
 
     def register(
         self,
-        compute_field: Callable[['ASTNode'], Any],
+        compute_field: Callable[["ASTNode"], Any],
         name: str,
-        *node_types: 'ASTNodeType',
+        *node_types: "ASTNodeType",
     ) -> None:
         for node_type in node_types:
             computed_fields = self._registry[node_type]
-            if name in computed_fields and not _ComputedFieldsRegistry._is_in_interactive_shell():
+            if (
+                name in computed_fields
+                and not _ComputedFieldsRegistry._is_in_interactive_shell()
+            ):
                 raise RuntimeError(
-                    f'Registry already has computed field '
-                    f'named "{name}" for node type {node_type}.'
+                    f"Registry already has computed field "
+                    f"named '{name}' for node type {node_type}."
                 )
 
             computed_fields[name] = compute_field
 
-    def get_fields(self, node_type: 'ASTNodeType') -> Dict[str, Callable[['ASTNode'], Any]]:
+    def get_fields(
+        self, node_type: "ASTNodeType"
+    ) -> Dict[str, Callable[["ASTNode"], Any]]:
         return self._registry[node_type]
 
     def clear(self) -> None:
@@ -57,7 +62,10 @@ class _ComputedFieldsRegistry:
 
     @staticmethod
     def _is_in_interactive_shell() -> bool:
-        return bool(getattr(sys, 'ps1', sys.flags.interactive))
+        """
+        Taken from comments to this answer https://stackoverflow.com/a/6879085/2129920
+        """
+        return bool(getattr(sys, "ps1", sys.flags.interactive))
 
 
 computed_fields_registry = _ComputedFieldsRegistry()
