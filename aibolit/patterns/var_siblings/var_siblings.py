e3e7521740253db3591692ab78fd1cdfbdad0db6
Vitaly Protasov <izakladno@yandex.ru>
2020-07-22 16:20:48 +0300

    fixed by comments

--- aibolit/patterns/var_siblings/var_siblings.py
+++ aibolit/patterns/var_siblings/var_siblings.py
@@ -35,29 +35,28 @@ class VarSiblings:
         assert node.node_type == ASTNodeType.METHOD_DECLARATION
         vars_lines: Dict[str, int] = {}
         for local_var_node in ast.get_proxy_nodes(ASTNodeType.LOCAL_VARIABLE_DECLARATION):
-            var_line = local_var_node.line
-            var_declaration = local_var_node.declarators[0]
-            var_name = var_declaration.name
-            if var_name not in vars_lines:
-                # to filter not complex names
-                temp_name = re.split('([A-Z][^A-Z]*)', var_name)
-                if len(temp_name) > 1:
-                    vars_lines[var_name] = var_line
+            for var_declaration in local_var_node.declarators:
+                var_name = var_declaration.name
+                if var_name not in vars_lines:
+                    # to filter not complex names
+                    temp_name = re.split('([A-Z][^A-Z]*)', var_name)
+                    if len(temp_name) > 1:
+                        vars_lines[var_name] = local_var_node.line
         return vars_lines
 
-    def _compare_nodes_names(self, node_name_1: str, node_name_2: str) -> bool:
+    def _is_names_close(self, node_name_1: str, node_name_2: str) -> bool:
         # here we want to find those names, which start with the same word
         # and the lenght of this word > 3
         common_names_prefix = re.split('([A-Z][^A-Z]*)', node_name_1)[0]
-        return node_name_2 != node_name_1 and \
-            node_name_2.startswith(common_names_prefix) and len(common_names_prefix) > 3
+        return node_name_2.startswith(common_names_prefix) and len(common_names_prefix) > 3
 
     def _find_sibling_vars(self, vars_info: Dict) -> List[int]:
         lines: List[int] = []
-        for node in vars_info:
-            for new_node in vars_info:
-                if self._compare_nodes_names(node, new_node) and vars_info[new_node] not in lines:
-                    lines.append(vars_info[new_node])
+        for node_name1 in vars_info:
+            for node_name2 in vars_info:
+                if node_name1 != node_name2 and \
+                   self._is_names_close(node_name1, node_name2) and vars_info[node_name2] not in lines:
+                    lines.append(vars_info[node_name2])
         return lines
 
     def value(self, filename: str) -> List[int]:
