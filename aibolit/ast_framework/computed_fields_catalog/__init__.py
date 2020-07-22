5739d40bc44659def86d7e8254f894ae6c13625c
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-07-22 16:40:59 +0300

    Chain computed fields.

--- aibolit/ast_framework/computed_fields_catalog/__init__.py
+++ aibolit/ast_framework/computed_fields_catalog/__init__.py
@@ -1,2 +1,3 @@
 # Import all computed fields in the catalog for registering them all in once
 import aibolit.ast_framework.computed_fields_catalog.node_filter  # noqa: F401
+import aibolit.ast_framework.computed_fields_catalog.chained_fields  # noqa: F401
