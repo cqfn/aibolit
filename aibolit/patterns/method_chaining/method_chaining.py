4845205eaf135b89a7c822e175d109eba5877d32
Vitaly Protasov <izakladno@yandex.ru>
2020-07-24 14:15:33 +0300

    refactored

--- aibolit/patterns/method_chaining/method_chaining.py
+++ aibolit/patterns/method_chaining/method_chaining.py
@@ -1,68 +1,29 @@
-import javalang
-
-import uuid
-from collections import defaultdict
-
+from aibolit.ast_framework import ASTNodeType, AST
 from aibolit.utils.ast_builder import build_ast
+from typing import List
+from aibolit.ast_framework.ast_node import ASTNode
 
 
 class MethodChainFind:
+    '''
+    In this pattern we check whether 
+    more than one method chaining 
+    invocation is used or not.
+    '''
+    def _check_chained_method(self, method_invocation: ASTNode) -> bool:
+        childs = 0
+        for node in method_invocation.children:
+            if node.node_type == ASTNodeType.METHOD_INVOCATION:
+                childs += 1
+        return childs > 1
 
-    def __init__(self):
-        pass
+    def value(self, filename: str) -> List[int]:
+        lines: List[int] = []
+        ast = AST.build_from_javalang(build_ast(filename))
+        for stetement_expression in ast.get_proxy_nodes(ASTNodeType.STATEMENT_EXPRESSION):
+            expression_child = stetement_expression.expression
+            if expression_child.node_type in [ASTNodeType.CLASS_CREATOR, ASTNodeType.METHOD_INVOCATION] and \
+               self._check_chained_method(expression_child):
+                lines.append(expression_child.line)
 
-    def traverse_node(self, node, dict_with_chains, uuid_method):
-        if not node:
-            return dict_with_chains
-
-        for item in node.children:
-            if item and (isinstance(item, tuple) or isinstance(item, list)):
-                for j in item:
-                    if isinstance(j, javalang.tree.MethodInvocation):
-                        if not j.qualifier and j.qualifier != '':
-                            # it means that there is method chaining
-                            dict_with_chains[uuid_method].append([j.position.line, j.member])
-                            self.traverse_node(j, dict_with_chains, uuid_method)
-                        else:
-                            # it means that we have method invocation without chaining like
-                            # result.add(field.getName(), column.columnName(), field.getType());
-                            new_uuid = uuid.uuid1()
-                            dict_with_chains[new_uuid].append([j.position.line, j.member])
-                            self.traverse_node(j, dict_with_chains, new_uuid)
-
-                    elif isinstance(j, javalang.tree.MethodDeclaration):
-                        self.traverse_node(j, dict_with_chains, str(uuid.uuid1()))
-
-                    elif isinstance(j, javalang.tree.StatementExpression):
-                        self.traverse_node(j, dict_with_chains, uuid_method)
-
-                    elif isinstance(j, javalang.tree.This) or isinstance(j, javalang.tree.ClassCreator):
-                        self.traverse_node(j, dict_with_chains, str(uuid.uuid1()))
-            elif isinstance(item, javalang.tree.ClassCreator):
-                self.traverse_node(item, dict_with_chains, uuid_method)
-
-        return dict_with_chains
-
-
-    # flake8: noqa: C901
-    def value(self, filename: str):
-        """
-        Travers over AST tree finds method chaining. It is searched in a statement
-        :param filename:
-        :return:
-        List of tuples with LineNumber and List of methods names, e.g.
-        [[10, 'func1'], [10, 'fun2']], [[23, 'run'], [23, 'start']]]
-        """
-        tree = build_ast(filename)
-        chain_lst = defaultdict(list)
-        for _, node in tree.filter(javalang.tree.StatementExpression):
-            if isinstance(node.expression, javalang.tree.MethodInvocation):
-                children = node.children
-                if isinstance(children[1], javalang.tree.MethodInvocation):
-                    uuid_first_method = str(uuid.uuid1())
-                    chain_lst[uuid_first_method].append([
-                        children[1].position.line, children[1].member])
-                    self.traverse_node(children[1], chain_lst, uuid_first_method)
-
-        filtered_dict = list(filter(lambda elem: len(elem) > 1, chain_lst.values()))
-        return [item[0][0] for item in filtered_dict]
+        return lines
