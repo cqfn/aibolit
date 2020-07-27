ba14922a8343daa334f97f04bcd8ebca824394bf
Vitaly Protasov <izakladno@yandex.ru>
2020-07-27 19:23:57 +0300

    refactored

--- aibolit/patterns/multiple_try/multiple_try.py
+++ aibolit/patterns/multiple_try/multiple_try.py
@@ -1,77 +1,40 @@
-import javalang
-from typing import List
-import uuid
-from collections import defaultdict
-import hashlib
-import itertools
-from javalang.tree import FormalParameter
-
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Aibolit
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+from aibolit.ast_framework import ASTNodeType, AST
 from aibolit.utils.ast_builder import build_ast
+from typing import List
+from aibolit.ast_framework.ast_node import ASTNode
 
 
 class MultipleTry:
+    '''
+    Check if a method contains more than one Try Statement
+    '''
+    def value(self, filename) -> List[int]:
+        total_code_lines: List[int] = []
+        ast = AST.build_from_javalang(build_ast(filename))
+        for method_declaration in ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION):
+            try_nodes = list(ast.get_subtree(method_declaration).get_proxy_nodes(ASTNodeType.TRY_STATEMENT))
+            if len(try_nodes) > 1:
+                total_code_lines.append(method_declaration.line)
 
-    def __init__(self):
-        pass
-
-    def traverse_node(self, node, dict_with_chains, uuid_method):
-        if not node:
-            return dict_with_chains
-
-        for item in node.children:
-            if item and (isinstance(item, tuple) or isinstance(item, list)):
-                for j in item:
-                    if isinstance(j, javalang.tree.MethodInvocation):
-                        if not j.qualifier and j.qualifier != '':
-                            # it means that there is method chaining
-                            dict_with_chains[uuid_method].append([j.position.line, j.member])
-                            self.traverse_node(j, dict_with_chains, uuid_method)
-                        else:
-                            # it means that we have method invocation without chaining like
-                            # result.add(field.getName(), column.columnName(), field.getType());
-                            new_uuid = uuid.uuid1()
-                            dict_with_chains[new_uuid].append([j.position.line, j.member])
-                            self.traverse_node(j, dict_with_chains, new_uuid)
-
-                    elif isinstance(j, javalang.tree.MethodDeclaration):
-                        self.traverse_node(j, dict_with_chains, str(uuid.uuid1()))
-
-                    elif isinstance(j, javalang.tree.StatementExpression):
-                        self.traverse_node(j, dict_with_chains, uuid_method)
-
-                    elif isinstance(j, javalang.tree.This) or isinstance(j, javalang.tree.ClassCreator):
-                        self.traverse_node(j, dict_with_chains, str(uuid.uuid1()))
-            elif isinstance(item, javalang.tree.ClassCreator):
-                self.traverse_node(item, dict_with_chains, uuid_method)
-
-        return dict_with_chains
-
-
-    # flake8: noqa: C901
-    def value(self, filename: str):
-        """
-        Travers over AST tree and fins function with nested/sequential try statement
-        :param filename:
-        :return:
-        List of tuples with LineNumber and List of methods names, e.g.
-        [[10, 'func1'], [10, 'fun2']], [[23, 'run'], [23, 'start']]]
-        """
-
-        tree = build_ast(filename)
-        res = defaultdict(list)
-        for _, method_node in tree.filter(javalang.tree.MethodDeclaration):
-            for _, try_node in method_node.filter(javalang.tree.TryStatement):
-                formal_params = [
-                    (x.type.name + ' ' + x.name)  
-                    for x in method_node.parameters 
-                    if isinstance(x, FormalParameter)
-                ]
-                func_name = '{f}({params})'.format(
-                    f=method_node.name,
-                    params=','.join(formal_params)
-                ).encode('utf-8')
-                m = hashlib.md5()
-                m.update(func_name)
-                res[m.hexdigest()].append(method_node.position.line)
-
-        return list(set(itertools.chain.from_iterable([y for x, y in res.items() if len(y) > 1])))
+        return total_code_lines
