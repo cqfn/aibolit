9dff327eb568574b3d77c318d95c0daddb90396a
Evgeny Maslov <lyriccoder@gmail.com>
2020-07-28 18:00:25 +0300

    Add implementation

--- aibolit/patterns/send_null/send_null.py
+++ aibolit/patterns/send_null/send_null.py
@@ -1,8 +1,8 @@
 import javalang
 from typing import List
 
+from aibolit.ast_framework import ASTNodeType, AST
 from aibolit.utils.ast_builder import build_ast
-
 
 class SendNull:
 
@@ -65,3 +65,41 @@ class SendNull:
                 pass
         lst = sorted(lst)
         return lst
+
+
+class SendNullFixed:
+
+    def __init__(self):
+        pass
+
+
+    def value(self, filename: str) -> List[int]:
+
+        lines = set()
+        tree = AST.build_from_javalang(build_ast(filename))
+        invocatios_types = [
+            ASTNodeType.METHOD_INVOCATION,
+            ASTNodeType.EXPLICIT_CONSTRUCTOR_INVOCATION,
+            ASTNodeType.CLASS_CREATOR
+        ]
+        # print(tree)
+        for node in tree.get_proxy_nodes(*invocatios_types):
+            # print(node.member, '------------------------------')
+            # if node.line > 3533:
+                # print(node)
+            for argument in node.arguments:
+                if (argument.node_type == ASTNodeType.LITERAL) and (argument.value == "null"):
+                    lines.add(argument.line)
+
+        for node in tree.get_proxy_nodes(ASTNodeType.TERNARY_EXPRESSION):
+            # if node.line > 1000:
+                # print(node)
+            if (hasattr(node.if_true, 'value') and node.if_true.value == 'null') or \
+                    (hasattr(node.if_false, 'value') and node.if_false.value == 'null'):
+                lines.add(node.line)
+
+        lst = sorted(lines)
+        return lst
+
+# a = SendNullFixed().value(r'D:\git\aibolit\test\patterns\send_null\Temp.java')
+# print(a)
