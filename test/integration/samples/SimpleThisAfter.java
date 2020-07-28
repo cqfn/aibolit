302c4ad800a323b099610d8cbf1df671aa6bc3c7
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-07-28 14:42:52 +0300

    [#528] Move files from partially sync method tests to integration.

---
+++ test/integration/samples/SimpleThisAfter.java
@@ -0,0 +1,39 @@
+/** An abstract IPC service.  IPC calls take a single {@link Writable} as a
+ * parameter, and return a {@link Writable} as their value.  A service runs on
+ * a port and is defined by a parameter class and a value class.
+ * 
+ * @see Client
+ */
+@Public
+@InterfaceStability.Evolving
+public abstract class Server {
+  private final boolean authorize;
+  private List<AuthMethod> enabledAuthMethods;
+  private RpcSaslProto negotiateResponse;
+  private ExceptionsHandler exceptionsHandler = new ExceptionsHandler();
+  private Tracer tracer;
+  private AlignmentContext alignmentContext;
+  /**
+   * Logical name of the server used in metrics and monitor.
+   */
+
+    private void doublthis(RpcCall call, long now) {
+	  
+		synchronized (this) {
+			Iterator<RpcCall> iter = responseQueue.listIterator(0);
+			synchronized (this) {
+				while (iter.hasNext()) {
+				  call = iter.next();
+				  if (now > call.responseTimestampNanos + PURGE_INTERVAL_NANOS) {
+					closeConnection(call.connection);
+					break;
+				  }
+				}
+			}
+		}
+	  while (1) {
+		  func();
+	  }
+  
+    }
+}
