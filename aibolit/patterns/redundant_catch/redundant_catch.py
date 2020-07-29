41cc471364db676cd4fe8344c2ab6540d212cbaa
Vitaly Protasov <izakladno@yandex.ru>
2020-07-29 15:04:29 +0300

    rewrote code according to Zenya comments

--- aibolit/patterns/redundant_catch/redundant_catch.py
+++ aibolit/patterns/redundant_catch/redundant_catch.py
@@ -32,7 +32,7 @@ class RedundantCatch:
     try -> catch structure in this method
     '''
 
-    def _is_redundant(self, method_throw_name: List[str], try_node: ASTNode):
+    def _is_redundant(self, method_throw_name: List[str], try_node: ASTNode) -> bool:
         assert try_node.node_type == ASTNodeType.TRY_STATEMENT
         for catch_node in try_node.catches:
             for catch_node_name in catch_node.parameter.types:
@@ -40,9 +40,16 @@ class RedundantCatch:
                     return True
         return False
 
+    def _get_lambda_try_nodes(self, ast: AST, lambda_node: ASTNode)  -> List[int]:
+        assert lambda_node.node_type == ASTNodeType.LAMBDA_EXPRESSION
+        return [try_node.line for try_node in \
+                ast.get_subtree(lambda_node).get_proxy_nodes(ASTNodeType.TRY_STATEMENT)]
+
     def value(self, filename: str) -> List[int]:
         lines: List[int] = []
+        excluded_nodes: List[int] = []
         ast = AST.build_from_javalang(build_ast(filename))
+
         for method_declaration in ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION,
                                                       ASTNodeType.CONSTRUCTOR_DECLARATION):
             method_throw_names = method_declaration.throws
@@ -52,4 +59,6 @@ class RedundantCatch:
                         self._is_redundant(method_throw_names, try_node):
                     lines.append(try_node.line)
 
-        return lines
+            for lambda_node in ast.get_subtree(method_declaration).get_proxy_nodes(ASTNodeType.LAMBDA_EXPRESSION):
+                excluded_nodes.extend(self._get_lambda_try_nodes(ast, lambda_node))
+        return sorted(list(set(lines).difference(set(excluded_nodes))))
