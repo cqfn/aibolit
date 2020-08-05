09309c5c83cec0c70747485fd3c91b511b45da6b
Evgeny Maslov <lyriccoder@gmail.com>
2020-08-05 14:25:49 +0300

    Fix types

--- aibolit/model/model.py
+++ aibolit/model/model.py
@@ -173,7 +173,7 @@ class PatternRankingModel(BaseEstimator):
 
         return (np.array(ranked), pairs[:, 0].T.tolist()[::-1])
 
-    def test(self, files: List[str]) -> Tuple[str, List[str], List[float]]:
+    def test(self, files: List[str]) -> List[List[str, List[str], List[float]]]:
         config = Config.get_patterns_config()
         patterns_config = config['patterns']
         metrics_config = config['metrics']
@@ -208,7 +208,7 @@ class PatternRankingModel(BaseEstimator):
         result_array = []
         for file_for_file in results:
             sorted_result, importances = self.predict(file_for_file)
-            result_array.append([file_for_file['filename'], sorted_result.keys(), importances])
+            result_array.append([file_for_file['filename'], list(sorted_result.keys()), importances])
 
         return result_array
 
