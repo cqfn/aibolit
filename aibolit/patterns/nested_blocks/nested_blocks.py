199b28087e23201c4120ade661d024a835117ce0
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-08-08 12:05:37 +0300

    [528] Make nested block pattern check all nested statements.

--- aibolit/patterns/nested_blocks/nested_blocks.py
+++ aibolit/patterns/nested_blocks/nested_blocks.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from typing import List, Optional
+from typing import List
 
 from aibolit.ast_framework import AST, ASTNode, ASTNodeType
 from aibolit.utils.ast_builder import build_ast
@@ -32,51 +32,59 @@ class NestedBlocks:
     nested blocks statements are located
     '''
 
-    def __init__(self, max_depth: int, *block_type: ASTNodeType):
+    def __init__(self, max_depth: int, *block_types: ASTNodeType):
         self._max_depth = max_depth
-        self._block_type = block_type
+        self._block_types = block_types
 
     def value(self, filename: str) -> List[int]:
         lines: List[int] = []
         ast = AST.build_from_javalang(build_ast(filename))
-        for node in ast.get_subtrees(*self._block_type):
-            nested_node = self._is_overnested(node.get_root())
-            if nested_node is not None:
-                lines.append(nested_node.line)
+        for block_type in self._block_types:
+            for block_ast in ast.get_subtrees(block_type):
+                for overnested_block in self._find_overnested_blocks(block_ast.get_root()):
+                    lines.append(overnested_block.line)
 
         return lines
 
-    def _is_overnested(self, node: ASTNode) -> Optional[ASTNode]:
-        node_type = node.node_type
-        nested_level = 1
-        block = self._get_next_block(node)
+    def _find_overnested_blocks(self, node: ASTNode, depth: int = 1) -> List[ASTNode]:
+        if depth == self._max_depth:
+            return [node]
 
-        while len(block) == 1 and block[0].node_type == node_type:
-            nested_level += 1
-            nested_statement = block[0]
-            if nested_level >= self._max_depth:
-                return nested_statement
+        overnested_blocks: List[ASTNode] = []
+        for nested_block in self._get_nested_statements(node):
+            if nested_block.node_type == node.node_type:
+                overnested_blocks.extend(self._find_overnested_blocks(nested_block, depth + 1))
 
-            block = self._get_next_block(nested_statement)
+        return overnested_blocks
 
-        return None
+    def _get_nested_statements(self, node: ASTNode) -> List[ASTNode]:
+        nested_statements: List[ASTNode] = []
 
-    def _get_next_block(self, node: ASTNode) -> List[ASTNode]:
         if node.node_type == ASTNodeType.IF_STATEMENT:
-            return self._wrap_single_statement_block(node.then_statement)
+            nested_statements.extend(self._get_block_statements_list(node.then_statement))
+
+            while node.else_statement is not None and node.else_statement.node_type == ASTNodeType.IF_STATEMENT:
+                node = node.else_statement
+                nested_statements.extend(self._get_block_statements_list(node.then_statement))
+
+            if node.else_statement is not None:
+                nested_statements.extend(self._get_block_statements_list(node.else_statement))
+
         elif node.node_type in {ASTNodeType.DO_STATEMENT,
                                 ASTNodeType.FOR_STATEMENT,
                                 ASTNodeType.WHILE_STATEMENT}:
-            return self._wrap_single_statement_block(node.body)
+            nested_statements.extend(self._get_block_statements_list(node.body))
 
-        raise RuntimeError(f"{node} was provided. Expected types are:\n"
-                           " - ASTNodeType.DO_STATEMENT\n"
-                           " - ASTNodeType.IF_STATEMENT\n"
-                           " - ASTNodeType.FOR_STATEMENT\n"
-                           " - ASTNodeType.WHILE_STATEMENT\n")
+        return nested_statements
 
-    def _wrap_single_statement_block(self, node: ASTNode) -> List[ASTNode]:
+    def _get_block_statements_list(self, node: ASTNode) -> List[ASTNode]:
         if node.node_type == ASTNodeType.BLOCK_STATEMENT:
             return node.statements
 
+        # A single statement is treated as a block with this statement
+        # This happens in following situations
+        # ```java
+        # while(True)
+        #     doSmth();
+        # ```
         return [node]
