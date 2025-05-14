44cfadcc2ef4fbef045219f6346883798fea53f5
Yegor Bugayenko <yegor256@gmail.com>
2025-05-14 16:38:29 +0300

    license fix

--- test/ast_framework/ncss/ImmersiveModeSampleTests.java
+++ test/ast_framework/ncss/ImmersiveModeSampleTests.java
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2019-2025 CQFN.org
+// SPDX-License-Identifier: MIT
+
 /*
  * Copyright (C) 2013 The Android Open Source Project
  *
--- test/patterns/send_null/AddOp.java
+++ test/patterns/send_null/AddOp.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2024-2025 Yegor Bugayenko
+// Copyright (c) 2019-2025 CQFN.org
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the 'Software'), to deal
