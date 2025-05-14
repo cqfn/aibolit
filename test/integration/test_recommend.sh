e819e71eddb52aea3e9aaeec6e69058b44819e57
Yegor Bugayenko <yegor256@gmail.com>
2025-05-14 16:55:46 +0300

    more licenses

--- test/integration/test_recommend.sh
+++ test/integration/test_recommend.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2019-2025 CQFN.org
+# SPDX-License-Identifier: MIT
+
 python3 -m aibolit check --folder ./test/integration/check/java
-if [ $? -eq 2 ]
-then
+
+if [ $? -eq 2 ]; then
   echo "Failure: aibolit check has failed."
   exit 1
-else
-  echo "Success: aibolit check was successful"
-  exit 0
 fi
+
+echo "Success: aibolit check was successful"
