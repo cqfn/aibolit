66548a864df3dc5ccdf874a2c3497160e6365595
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-23 14:59:01 +0700

    FIX: typing for build_ast

--- aibolit/utils/ast_builder.py
+++ aibolit/utils/ast_builder.py
@@ -1,12 +1,14 @@
 # SPDX-FileCopyrightText: Copyright (c) 2019-2025 Aibolit
 # SPDX-License-Identifier: MIT
+import os
+
 from javalang.parse import parse
 from javalang.tree import CompilationUnit
 
 from aibolit.utils.encoding_detector import read_text_with_autodetected_encoding
 
 
-def build_ast(filename: str) -> CompilationUnit:
+def build_ast(filename: str | os.PathLike) -> CompilationUnit:
     return parse(read_text_with_autodetected_encoding(filename))
 
 
