758eb84a2db18c502664335cccc36494d5816b1e
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-23 14:54:06 +0700

    FIX: typing for detect_encoding_of_file

--- aibolit/utils/encoding_detector.py
+++ aibolit/utils/encoding_detector.py
@@ -1,9 +1,11 @@
 # SPDX-FileCopyrightText: Copyright (c) 2019-2025 Aibolit
 # SPDX-License-Identifier: MIT
+import os
+
 from chardet import detect  # type: ignore
 
 
-def detect_encoding_of_file(filename: str):
+def detect_encoding_of_file(filename: str | os.PathLike):
     with open(filename, 'rb') as target_file:
         return detect_encoding_of_data(target_file.read())
 
