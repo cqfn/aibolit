91d1a44fed416d8687d0b827430b345ff47dba81
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-25 14:53:21 +0700

    FIX: indentation in ExternalMethodsCalled docstring

--- aibolit/metrics/external_methods_called/external_methods_called.py
+++ aibolit/metrics/external_methods_called/external_methods_called.py
@@ -5,12 +5,13 @@ import os
 
 class ExternalMethodsCalled:
     """
-        Measure the number of external methods called by the class.
-        @todo #183:30min Implement external method called metric
-         In the methods in the class an external method for this class can
-         be called. So number of external methods called by all the methods
-         in the class is suggested as a metric. Implement this metric and then
-         enable tests in test_external_methods_called.py
+    Measure the number of external methods called by the class.
+
+    @todo #183:30min Implement external method called metric
+    In the methods in the class an external method for this class can
+    be called. So number of external methods called by all the methods
+    in the class is suggested as a metric. Implement this metric and then
+    enable tests in test_external_methods_called.py
     """
 
     def __init__(self):
