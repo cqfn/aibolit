d3035f46e884df5867608df72d652ad68ceb1817
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-25 15:00:25 +0700

    FIX: indent in MutableIndex docstring

--- aibolit/patterns/mutable_index/mutable_index.py
+++ aibolit/patterns/mutable_index/mutable_index.py
@@ -11,11 +11,13 @@ class MutableIndex:
     def value(self, filename: str | os.PathLike):
         """
         Traverse over AST tree and finds loops with mutable index
+
         :return:
         List of line number of loops with mutable index
+
         @todo #147:30min Implement MutableIndex pattern
-         We need to implement the code on mutable index pattern. It must return the number
-         of the lines where we have an index of a for loop being mutated. After implementing,
-         the method, enable all tests in test_mutable_index.py
+        We need to implement the code on mutable index pattern. It must return the number
+        of the lines where we have an index of a for loop being mutated. After implementing,
+        the method, enable all tests in test_mutable_index.py
         """
         return []
