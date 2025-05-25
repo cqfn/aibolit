1232db5d6c7e6ae52b5c3bc6600593f2b0126940
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-25 15:01:01 +0700

    FIX: indent in NonFinalArgument

--- aibolit/patterns/non_final_argument/non_final_argument.py
+++ aibolit/patterns/non_final_argument/non_final_argument.py
@@ -13,11 +13,13 @@ class NonFinalArgument:
     def value(self, filename: str | os.PathLike) -> List[LineNumber]:
         """
         Returns the line numbers of the file name which contains no final arguments
+
         :param filename: name of the file
         :return: number of the lines with non-final arguments
+
         @todo #146:30min Implement NonFinalArgument pattern.
-         All method arguments must have final modifiers. Once we find an argument
-         without final, it's a pattern. After implementing that, enable tests in
-         test_non_final_argument.py
+        All method arguments must have final modifiers. Once we find an argument
+        without final, it's a pattern. After implementing that, enable tests in
+        test_non_final_argument.py
         """
         return []
