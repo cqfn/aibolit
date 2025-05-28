e14659c31a4c2aa5794a94af6b236c185b1f25d7
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-28 21:08:24 +0700

    Refactor by extracting a helper iterator

--- aibolit/patterns/non_final_argument/non_final_argument.py
+++ aibolit/patterns/non_final_argument/non_final_argument.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2019-2025 Aibolit
 # SPDX-License-Identifier: MIT
 import os
-from typing import List
+from typing import Iterator
 
-from aibolit.ast_framework import AST, ASTNodeType
+from aibolit.ast_framework import AST, ASTNode, ASTNodeType
 from aibolit.types_decl import LineNumber
 from aibolit.utils.ast_builder import build_ast
 
@@ -12,20 +12,24 @@ class NonFinalArgument:
     def __init__(self):
         pass
 
-    def value(self, filename: str | os.PathLike) -> List[LineNumber]:
+    def value(self, filename: str | os.PathLike) -> list[LineNumber]:
         """
         Returns the line numbers of the file name which contains no final arguments
 
         :param filename: name of the file
         :return: number of the lines with non-final arguments
         """
-        ast = AST.build_from_javalang(build_ast(filename))
-        lines: list[int] = []
-        for node in ast.get_proxy_nodes(
-            ASTNodeType.METHOD_DECLARATION,
-            ASTNodeType.CONSTRUCTOR_DECLARATION,
-        ):
-            for parameter in node.parameters:
-                if 'final' not in parameter.modifiers:
-                    lines.append(node.line)
-        return sorted(lines)
+        return sorted(
+            node.line
+            for node in _offending_nodes(AST.build_from_javalang(build_ast(filename)))
+        )
+
+
+def _offending_nodes(ast: AST) -> Iterator[ASTNode]:
+    for node in ast.get_proxy_nodes(
+        ASTNodeType.METHOD_DECLARATION,
+        ASTNodeType.CONSTRUCTOR_DECLARATION,
+    ):
+        for parameter in node.parameters:
+            if 'final' not in parameter.modifiers:
+                yield node
