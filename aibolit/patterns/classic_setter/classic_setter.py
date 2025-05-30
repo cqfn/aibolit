a4eb390454cb91f834bc19074e866841a9eac0df
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-30 15:23:47 +0700

    Refactor solution

--- aibolit/patterns/classic_setter/classic_setter.py
+++ aibolit/patterns/classic_setter/classic_setter.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2019-2025 Aibolit
 # SPDX-License-Identifier: MIT
-from typing import List
+from typing import Iterator, List
 from aibolit.ast_framework import ASTNodeType, AST
 from aibolit.ast_framework.ast_node import ASTNode
 
@@ -11,19 +11,25 @@ class ClassicSetter:
     assigning in the method. Also, asserts are ignored.
     '''
     def value(self, ast: AST) -> List[int]:
-        lines: List[int] = []
-        for node in ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION):
-            method_name = node.name
-            if not node.parameters:
-                continue
+        return sorted(set(node.line for node in _setter_nodes(ast)))
 
-            parameter_name = node.parameters[0].name
-            if node.return_type is None and method_name.startswith('set'):
-                for child in node.children:
-                    if child.node_type == ASTNodeType.STATEMENT_EXPRESSION:
-                        expression = child.expression
-                        if expression.node_type == ASTNodeType.ASSIGNMENT and expression.value.member == parameter_name:
-                            lines.append(node.line)
 
-                lines.append(node.line)
-        return sorted(set(lines))
+def _setter_nodes(ast: AST) -> Iterator[ASTNode]:
+    for node, parameters in _methods_with_parameters(ast):
+        method_name = node.name
+        parameter_name = node.parameters[0].name
+        if node.return_type is None and method_name.startswith('set'):
+            for child in node.children:
+                if child.node_type == ASTNodeType.STATEMENT_EXPRESSION:
+                    expression = child.expression
+                    if (
+                        expression.node_type == ASTNodeType.ASSIGNMENT and
+                        expression.value.member == parameter_name
+                    ):
+                        yield node
+
+
+def _methods_with_parameters(ast: AST) -> Iterator[tuple[ASTNode, list[str]]]:
+    for node in ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION):
+        if node.parameters:
+            yield node, node.parameters
