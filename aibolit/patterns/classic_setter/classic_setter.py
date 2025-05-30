a235a9d03a055fba8bb3a03275da376beab7c468
Maxim Ivanov <ivanovmg@gmail.com>
2025-05-30 14:11:30 +0700

    Drop unnecessary code - tests pass without it

--- aibolit/patterns/classic_setter/classic_setter.py
+++ aibolit/patterns/classic_setter/classic_setter.py
@@ -10,26 +10,10 @@ class ClassicSetter:
     The method's name starts with set. There are attributes
     assigning in the method. Also, asserts are ignored.
     '''
-    suitable_nodes: List[ASTNodeType] = [
-        ASTNodeType.ASSERT_STATEMENT,
-        ASTNodeType.STATEMENT_EXPRESSION,
-    ]
-
-    def _check_body_nodes(self, check_setter_body: List[ASTNode]) -> bool:
-        '''
-        Check whether nodes are agree with the following types
-        (in self.suitable_nodes) or not.
-        '''
-        for node in check_setter_body:
-            if node.node_type not in self.suitable_nodes:
-                return False
-        return True
-
     def value(self, ast: AST) -> List[int]:
         lines: List[int] = []
         for node in ast.get_proxy_nodes(ASTNodeType.METHOD_DECLARATION):
             method_name = node.name
-            if node.return_type is None and method_name.startswith('set') and \
-                    self._check_body_nodes(node.body):
+            if node.return_type is None and method_name.startswith('set'):
                 lines.append(node.line)
         return sorted(lines)
