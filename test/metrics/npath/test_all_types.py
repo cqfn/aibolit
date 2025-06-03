b427ce29205f6beada69c22df1b7d3027f00f279
Maxim Ivanov <ivanovmg@gmail.com>
2025-06-03 14:14:46 +0700

    Add example on if/else with complexity of 5

--- test/metrics/npath/test_all_types.py
+++ test/metrics/npath/test_all_types.py
@@ -165,6 +165,32 @@ class TestMvnFreeNPathMetric:
         ).strip()
         assert self._value(content) == 4
 
+    def test_complex_if_else_with_npath_complexity_of_5(self) -> None:
+        content = dedent(
+            """\
+            public class NPath5Example {
+                public void checkValues(int a, int b) {
+                    if (a > 0) { // Branch 1 (2 paths: true/false)
+                        if (b > 0) { // Branch 2 (2 paths)
+                            System.out.println("a > 0 and b > 0");
+                        } else {
+                            System.out.println("a > 0 but b <= 0");
+                        }
+                    } else { // Branch 3 (1 path, but 3 sub-paths)
+                        if (b > 0) {
+                            System.out.println("a <= 0 but b > 0");
+                        } else if (b == 0) {
+                            System.out.println("a <= 0 and b == 0");
+                        } else {
+                            System.out.println("a <= 0 and b < 0");
+                        }
+                    }
+                }
+            }
+            """
+        ).strip()
+        assert self._value(content) == 5
+
     def test_switch_simple(self):
         content = dedent(
             """\
