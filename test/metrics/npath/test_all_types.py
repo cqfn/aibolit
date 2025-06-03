0704363c34ac63fe2d85384afb86caff946a181c
Maxim Ivanov <ivanovmg@gmail.com>
2025-06-03 14:38:26 +0700

    Add failing test on nested switch

--- test/metrics/npath/test_all_types.py
+++ test/metrics/npath/test_all_types.py
@@ -225,6 +225,29 @@ class TestMvnFreeNPathMetric:
         )
         assert self._value(content) == 4
 
+    def test_nested_switch_statements(self) -> None:
+        content = dedent(
+            """\
+            class NestedSwitchStatements {
+                public void process(int x, int y) {
+                    switch (x) { // 1 path through case and 4 - default
+                        case 1:
+                            System.out.println("X=1");
+                            break;
+                        default:
+                            switch (y) {
+                                case 1: System.out.println("Y=1"); break;
+                                case 2: System.out.println("Y=2"); break;
+                                case 3: System.out.println("Y=3"); break;
+                                default: System.out.println("Y=default"); break;
+                            }
+                    }
+                }
+            }
+            """
+        ).strip()
+        assert self._value(content) == 5
+
     def _value(self, content: str) -> int:
         return MvnFreeNPathMetric(
             AST.build_from_javalang(
