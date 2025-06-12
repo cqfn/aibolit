5c0c5dc97bd04394086494b0b9ecab890a47fe4d
Alchemmist <anton.ingrish@gmail.com>
2025-06-12 13:25:27 +0300

    add test to graceful shutdown

---
+++ test/interface/test_graceful_shutdown.py
@@ -0,0 +1,14 @@
+import subprocess
+import time
+import signal
+
+
+def test_absence_traceback():
+    proc = subprocess.Popen(
+        ["python", "aibolit"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    time.sleep(2)
+    proc.send_signal(signal.SIGINT)
+    stdout, _ = proc.communicate(timeout=3)
+
+    assert b"Traceback" not in stdout
