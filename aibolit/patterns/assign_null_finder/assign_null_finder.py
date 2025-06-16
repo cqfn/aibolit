ec7ee4b1f2b6ddbc90ee8abf896ceb9bfe436865
Anton Prokopyev <aprokopyev161@gmail.com>
2025-06-16 10:44:12 +0300

    revert _is_null_assignment

--- aibolit/patterns/assign_null_finder/assign_null_finder.py
+++ aibolit/patterns/assign_null_finder/assign_null_finder.py
@@ -11,18 +11,13 @@ from aibolit.types_decl import LineNumber
 class NullAssignment:
     def value(self, ast: AST) -> List[LineNumber]:
         lines = set()
-        for node in ast.get_proxy_nodes(ASTNodeType.ASSIGNMENT, ASTNodeType.VARIABLE_DECLARATOR):
-            if self._is_null_assignment(node):
-                lines.add(node.line)
+        for assignment in ast.get_proxy_nodes(ASTNodeType.ASSIGNMENT):
+            if self._is_null_literal(assignment.value):
+                lines.add(assignment.line)
+        for declarator in ast.get_proxy_nodes(ASTNodeType.VARIABLE_DECLARATOR):
+            if self._is_null_literal(declarator.initializer):
+                lines.add(declarator.line)
         return sorted(lines)
-
-    def _is_null_assignment(self, node: ASTNode) -> bool:
-        return (
-            node.node_type == ASTNodeType.ASSIGNMENT and
-            self._is_null_literal(node.value) or
-            node.node_type == ASTNodeType.VARIABLE_DECLARATOR and
-            self._is_null_literal(node.initializer)
-        )
 
     def _is_null_literal(self, node: ASTNode | None) -> bool:
         return node is not None and node.node_type == ASTNodeType.LITERAL and node.value == 'null'
