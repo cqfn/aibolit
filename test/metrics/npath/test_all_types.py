9f460b100673d2b7de85b4aafdc6499fd73c53dc
Maxim Ivanov <ivanovmg@gmail.com>
2025-07-09 14:56:35 +0700

    TST: while with or binary condition

--- test/metrics/npath/test_all_types.py
+++ test/metrics/npath/test_all_types.py
@@ -461,6 +461,25 @@ class TestMvnFreeNPathMetric:
         ''').strip()
         assert self._value(content) == 3
 
+    def test_while_with_or_condition(self) -> None:
+        content = dedent('''
+        public class Test {
+            void whileWithOrCondition(int x, int y) {
+                while (x > 0 || y > 0) {
+                    if (x > 0) {
+                        System.out.println("X is positive");
+                    }
+                    if (y > 0) {
+                        System.out.println("Y is positive");
+                    }
+                    x--;
+                    y--;
+                }
+            }
+        }
+        ''').strip()
+        assert self._value(content) == 6
+
     def _value(self, content: str) -> int:
         return MvnFreeNPathMetric(
             AST.build_from_javalang(
