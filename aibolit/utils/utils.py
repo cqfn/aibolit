4ee9bea8b8655f6dc1ac8f8be37ea909f2caab99
Ilya Prokofiev <125394064+somepatt@users.noreply.github.com>
2026-06-10 16:11:01 +0300

    Refactor RemoveComments state machine helpers

--- aibolit/utils/utils.py
+++ aibolit/utils/utils.py
@@ -8,86 +8,90 @@ class RemoveComments:
         pass
 
     @staticmethod
+    def _trim_comment_only_indent(result):
+        while result and result[-1] not in '\n\r':
+            if result[-1] not in ' \t':
+                return
+            result.pop()
+
+    @staticmethod
+    def _consume_comment(symbol, next_symbol, result, state):
+        if state['mode'] == 'line_comment':
+            if symbol == '\n':
+                result.append(symbol)
+                state['mode'] = None
+            return 1
+        if state['mode'] == 'block_comment':
+            if symbol == '*' and next_symbol == '/':
+                state['mode'] = None
+                return 2
+            if symbol == '\n':
+                result.append(symbol)
+            return 1
+        return 0
+
+    @staticmethod
+    def _consume_literal(symbol, state):
+        if state['mode'] not in {'string', 'char'}:
+            return False
+        if state['escaped']:
+            state['escaped'] = False
+        elif symbol == '\\':
+            state['escaped'] = True
+        elif symbol == '"' and state['mode'] == 'string':
+            state['mode'] = None
+        elif symbol == "'" and state['mode'] == 'char':
+            state['mode'] = None
+        return True
+
+    @staticmethod
+    def _enter_literal(symbol, state):
+        if symbol == '"':
+            state['mode'] = 'string'
+            state['escaped'] = False
+            return True
+        if symbol == "'":
+            state['mode'] = 'char'
+            state['escaped'] = False
+            return True
+        return False
+
+    @staticmethod
+    def _enter_comment(symbol, next_symbol, result, state):
+        if symbol != '/' or next_symbol not in {'/', '*'}:
+            return 0
+        result.pop()
+        RemoveComments._trim_comment_only_indent(result)
+        state['mode'] = 'line_comment' if next_symbol == '/' else 'block_comment'
+        return 2
+
+    @staticmethod
     def remove_comments(string):
         result = []
+        state = {'mode': None, 'escaped': False}
         index = 0
-        in_string = False
-        in_char = False
-        in_line_comment = False
-        in_block_comment = False
-        escaped = False
-
-        def trim_comment_only_indent():
-            while result and result[-1] not in '\n\r':
-                if result[-1] not in ' \t':
-                    return
-                result.pop()
-
         while index < len(string):
             symbol = string[index]
             next_symbol = string[index + 1] if index + 1 < len(string) else ''
 
-            if in_line_comment:
-                if symbol == '\n':
-                    result.append(symbol)
-                    in_line_comment = False
-                index += 1
-                continue
-
-            if in_block_comment:
-                if symbol == '*' and next_symbol == '/':
-                    in_block_comment = False
-                    index += 2
-                    continue
-                if symbol == '\n':
-                    result.append(symbol)
-                index += 1
+            step = RemoveComments._consume_comment(symbol, next_symbol, result, state)
+            if step:
+                index += step
                 continue
 
             result.append(symbol)
 
-            if in_string:
-                if escaped:
-                    escaped = False
-                elif symbol == '\\':
-                    escaped = True
-                elif symbol == '"':
-                    in_string = False
+            if RemoveComments._consume_literal(symbol, state):
                 index += 1
                 continue
 
-            if in_char:
-                if escaped:
-                    escaped = False
-                elif symbol == '\\':
-                    escaped = True
-                elif symbol == "'":
-                    in_char = False
+            if RemoveComments._enter_literal(symbol, state):
                 index += 1
                 continue
 
-            if symbol == '"':
-                in_string = True
-                index += 1
-                continue
-
-            if symbol == "'":
-                in_char = True
-                index += 1
-                continue
-
-            if symbol == '/' and next_symbol == '/':
-                result.pop()
-                trim_comment_only_indent()
-                in_line_comment = True
-                index += 2
-                continue
-
-            if symbol == '/' and next_symbol == '*':
-                result.pop()
-                trim_comment_only_indent()
-                in_block_comment = True
-                index += 2
+            step = RemoveComments._enter_comment(symbol, next_symbol, result, state)
+            if step:
+                index += step
                 continue
 
             index += 1
