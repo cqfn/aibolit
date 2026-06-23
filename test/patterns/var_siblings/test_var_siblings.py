64373a58e119157b7b5f7a842a658d312402e428
Kolesnikov Arsenii <3451445@gmail.com>
2026-06-24 01:53:39 +0300

    Update test_var_siblings.py

--- test/patterns/var_siblings/test_var_siblings.py
+++ test/patterns/var_siblings/test_var_siblings.py
@@ -26,13 +26,13 @@ class VarSiblingsTestCase(unittest.TestCase):
         lines = pattern.value(ast)
         self.assertEqual(lines, [6, 8])
 
-    def test_find_length_4_var_siblings(self):
+        def test_find_length_4_var_siblings(self):
         filepath = self.current_directory / 'ShortVarSiblings.java'
         ast = AST.build_from_javalang(build_ast(filepath))
         pattern = VarSiblings()
         lines = pattern.value(ast)
         self.assertEqual(lines, [12, 13])
-        
+
     def test_siblings_scoped_to_method_not_whole_ast(self):
         filepath = self.current_directory / 'DisjointVarSiblingsMethods.java'
         ast = AST.build_from_javalang(build_ast(filepath))
