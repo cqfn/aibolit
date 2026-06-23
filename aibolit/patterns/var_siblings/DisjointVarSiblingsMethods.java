5cf0aea4ec0343de239ca0deb9631689687bbb32
Kolesnikov Arsenii <3451445@gmail.com>
2026-06-24 01:35:18 +0300

    Create DisjointVarSiblingsMethods.java

---
+++ aibolit/patterns/var_siblings/DisjointVarSiblingsMethods.java
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: Copyright (c) 2019-2026 Aibolit
+// SPDX-License-Identifier: MIT
+class DisjointVarSiblingsMethods {
+    void firstMethod() {
+        String customerName = "a";
+        String customerAddress = "b";
+    }
+    void secondMethod() {
+        String productName = "x";
+        String productPrice = "y";
+    }
+}
