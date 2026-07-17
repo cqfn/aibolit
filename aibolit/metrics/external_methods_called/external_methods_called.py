cb351418ffd0249b96825301b822999a8867fa4e
Beeeeeeee-c <Beeeeeeee-c@users.noreply.github.com>
2026-07-17 16:25:04 +0300

    Implement external methods called metric

--- aibolit/metrics/external_methods_called/external_methods_called.py
+++ aibolit/metrics/external_methods_called/external_methods_called.py
@@ -1,11 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2019-2026 Aibolit
 # SPDX-License-Identifier: MIT
-import itertools
-import os
 from dataclasses import dataclass
-from typing import Iterator
+from typing import Set
 
-from aibolit.ast_framework import AST, ASTNode, ASTNodeType
+from aibolit.ast_framework import AST, ASTNodeType
 from aibolit.utils.ast_builder import build_ast
 
 
@@ -14,11 +12,10 @@ class ExternalMethodsCalled:
     Measure the number of external methods called by the class.
     """
 
-    def __init__(self):
-        pass
-
-    def value(self, filepath: str | os.PathLike):
-        return ExternalMethodsCalledCount(AST.build_from_javalang(build_ast(filepath))).total()
+    def value(self, ast: AST) -> int:
+        if not isinstance(ast, AST):
+            ast = AST.build_from_javalang(build_ast(ast))
+        return ExternalMethodsCalledCount(ast).total()
 
 
 @dataclass(frozen=True)
@@ -28,23 +25,16 @@ class ExternalMethodsCalledCount:
     def total(self) -> int:
         return len(set(self._external_method_names_called()))
 
-    def _external_method_names_called(self) -> Iterator[str]:
-        for node in self._parent_method_invocation_nodes():
-            for first, second in itertools.pairwise(node.children):
-                if self._member_reference_followed_by_method_invocation(first, second):
-                    yield second.member
+    def _external_method_names_called(self) -> Set[str]:
+        local_methods = self._local_method_names()
+        return set(
+            node.member
+            for node in self.ast.proxy_nodes(ASTNodeType.METHOD_INVOCATION)
+            if node.member not in local_methods
+        )
 
-    def _parent_method_invocation_nodes(self) -> Iterator[ASTNode]:
-        for node in self.ast.proxy_nodes(ASTNodeType.METHOD_INVOCATION):
-            if (parent := node.parent) is not None:
-                yield parent
-
-    def _member_reference_followed_by_method_invocation(
-        self,
-        first: ASTNode,
-        second: ASTNode,
-    ) -> bool:
-        return (
-            first.node_type == ASTNodeType.MEMBER_REFERENCE and
-            second.node_type == ASTNodeType.METHOD_INVOCATION
+    def _local_method_names(self) -> Set[str]:
+        return set(
+            node.name
+            for node in self.ast.proxy_nodes(ASTNodeType.METHOD_DECLARATION)
         )
