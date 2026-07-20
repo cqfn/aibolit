5614e36f63525e39c207b04641bd2016a574eb52
Arinapakalova <66132801+Arinapakalova@users.noreply.github.com>
2026-07-20 16:00:57 +0300

    fix: add missing SPDX copyright headers to Java fixtures

--- test/patterns/return_empty_string/Multiple.java
+++ test/patterns/return_empty_string/Multiple.java
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2019-2026 Aibolit
+// SPDX-License-Identifier: MIT
+
 public class Multiple {
     public String first() {
         return "";
--- test/patterns/return_empty_string/NotEmpty.java
+++ test/patterns/return_empty_string/NotEmpty.java
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2019-2026 Aibolit
+// SPDX-License-Identifier: MIT
+
 public class NotEmpty {
     public String getText() {
         return "Hello World";
