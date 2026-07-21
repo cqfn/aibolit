ca92399fbcf914a4d4aedfb90dcd321f430b6baf
Arinapakalova <66132801+Arinapakalova@users.noreply.github.com>
2026-07-21 19:56:12 +0300

    #1312: add lambda expression edge case for P13 Null check

--- test/patterns/null_check/test_null_check.py
+++ test/patterns/null_check/test_null_check.py
@@ -46,6 +46,7 @@ class NullCheckTestCase(TestCase):
         pattern = NullCheck()
         lines = pattern.value(ast)
         self.assertEqual(lines, [7])
+        
     def test_null_check_in_lambda(self):
         filepath = self.current_directory / '6.java'
         ast = AST.build_from_javalang(build_ast(filepath))
